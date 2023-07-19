794e06e75394777f96d52ff9861f140184c469b4
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-07-19 06:43:31 +0000

    Wed, Jul 19, 2023, 11:13 AM +04:30

---
+++ CS50x/PSET1/population/population.c
@@ -0,0 +1,14 @@
+#incluce <stdio.h>
+#incluce <cs50.h>
+
+int main (void)
+
+{
+    //First population size
+     int First;
+  do
+  {
+      First = get_int("First population size: \n");
+  }
+  while (n < 1);
+}
