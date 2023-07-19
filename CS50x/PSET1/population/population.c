6645865052bd0ccd5396f56c4a5665844ecaf1c9
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-07-19 07:11:31 +0000

    Wed, Jul 19, 2023, 11:41 AM +04:30

--- CS50x/PSET1/population/population.c
+++ CS50x/PSET1/population/population.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void)
+int main(void)
 
 {
     //First population size
-        int First;
+    int First;
     do
     {
         First = get_int("First population size: \n");
@@ -13,18 +13,18 @@ int main (void)
     while (First < 9);
 
     //Second population size
-        int Second;
+    int Second;
     do
     {
         Second = get_int("Second population size: \n");
     }
-    while ( Second<First);
+    while (Second < First);
 
     //Calculation of the years that it takes for the First population to reach the Second population
-        int years=0;
-    while (First<Second)
+    int years=0;
+    while (First < Second)
     {
-    First=First+(First/3)-(First/4);
+        First=First+(First/3)-(First/4);
     years++;
     }
     //Showing the Years
