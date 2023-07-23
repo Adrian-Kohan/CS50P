ef155d83b776958c98c4713f03b6bb6a877b5d9e
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-07-23 07:50:32 +0000

    Sun, Jul 23, 2023, 12:20 PM +04:30

--- CS50x/PSET1/credit/credit.c
+++ CS50x/PSET1/credit/credit.c
@@ -8,15 +8,22 @@ long Visa (long card)
 
 int main(void)
 {
-    long Card-number = get_card ()
-    long American-express = AMEX (card)
-    long Master-Card = MasC (card)
-    long Visa-Card = Visa (card)
+    long Card-number = get_card ();
+    long American-express = AMEX (card);
+    long Master-Card = MasC (card);
+    long Visa-Card = Visa (card);
 }
 
 long get_card (void)
 {
-
+    int H;
+    do
+    {
+        H = get_int("How many cents are you owed?\n");
+    }
+    while (H < 0);
+    return H;
+}
 }
 long AMEX (long card)
 {
@@ -28,5 +35,5 @@ long MasC (long card)
 }
 long Visa (long card)
 {
-    
+
 }
