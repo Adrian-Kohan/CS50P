9f35c9846e19667ec555ca1a40a5076276896b57
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-07-23 07:55:22 +0000

    Sun, Jul 23, 2023, 12:25 PM +04:30

--- CS50x/PSET1/credit/credit.c
+++ CS50x/PSET1/credit/credit.c
@@ -1,17 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-long get_card (void)
-long AMEX (long card)
-long MasC (long card)
-long Visa (long card)
+long get_card (void);
+long AMEX (long card);
+long MasC (long card);
+long Visa (long card);
 
 int main(void)
 {
-    long Card-number = get_card ();
-    long American-express = AMEX (card);
-    long Master-Card = MasC (card);
-    long Visa-Card = Visa (card);
+    long card = get_card ();
+    long American_express = AMEX (card);
+    long Master_Card = MasC (card);
+    long Visa_Card = Visa (card);
 }
 
 long get_card (void)
