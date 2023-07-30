1d5230368acbe4e02e3b4fa37aba10411aa48963
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-07-30 08:25:51 +0000

    Sun, Jul 30, 2023, 12:55 PM +04:30

--- CS50x/PSET2/scrabble/scrabble.c
+++ CS50x/PSET2/scrabble/scrabble.c
@@ -41,7 +41,7 @@ int compute_score(string word)
     int j = 0;
     int l[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     int u[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    for (i = 0 , j = 0; i < 26 && j < strlen(word);  i++&& j++ )
+    for (i = 0; i < 26;  i++ )
     {
         u[i] = l[i];
         l[i] = POINTS[i];
