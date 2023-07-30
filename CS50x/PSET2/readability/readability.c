98310f6f2e4c742325603696dc623a5d117f95f0
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-07-30 20:01:49 +0000

    Mon, Jul 31, 2023, 12:31 AM +04:30

--- CS50x/PSET2/readability/readability.c
+++ CS50x/PSET2/readability/readability.c
@@ -34,7 +34,7 @@ int main(void)
     int L = numC / numW * 100;
     int S = numS / numW * 100;
     int index = 0.0588 * L - 0.296 * S - 15.8;
-    if (index <= 16)
+    if (index >= 16)
     {
         printf("Grade 16+\n");
     }
