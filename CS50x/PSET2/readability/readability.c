f45ed025076e7fba8a29032b118a76dbf5b3c168
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-07-30 14:45:35 +0000

    Sun, Jul 30, 2023, 7:15 PM +04:30

--- CS50x/PSET2/readability/readability.c
+++ CS50x/PSET2/readability/readability.c
@@ -1,8 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_word(int word)
 int main(void)
 {
     string text = get_string ("Write some text\n");
     printf("%s\n", text);
 }
+
+int count_word(int word)
