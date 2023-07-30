79efa2b04fe768874087c34758ed41fd17ed13c1
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-07-30 06:27:47 +0000

    Sun, Jul 30, 2023, 10:57 AM +04:30

---
+++ CS50x/PSET2/scrabble/scrabble.c
@@ -0,0 +1,54 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!%i %i\n", score1, score2);
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!%i %i\n", score1, score2);
+    }
+    else
+    {
+        printf("Tie!%i %i\n", score1, score2);
+    }
+}
+
+int compute_score(string word)
+{
+    //Compute and return score for string
+    int score = 0;
+    int l[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    int u[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    for (int i = 0 ; i < 26 ; i++ )
+    {
+        u[i] = l[i];
+        l[i] = POINTS[i];
+    }
+
+    for (int j = 0; j < strlen(word); j++ )
+    {
+        score = score + u[word[j]];
+    }
+
+    return score;
+}
