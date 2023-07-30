1358625ec523643e7b7710226672bc7273134d04
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-07-30 16:56:39 +0000

    Sun, Jul 30, 2023, 9:26 PM +04:30

--- CS50x/PSET2/readability/readability.c
+++ CS50x/PSET2/readability/readability.c
@@ -32,7 +32,7 @@ int main(void)
         }
     }
     int L = numC / numW * 100;
-    int S = numW / numS * 100;
+    int S = numS / numW * 100;
     int index = 0.0588 * L - 0.296 * S - 15.8;
 
     printf("%i %i %i\n", numC, numW, numS);
