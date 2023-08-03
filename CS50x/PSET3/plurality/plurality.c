ff9ce175f3a5351fd2b1b83329147ff252ef28ed
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-08-03 07:02:26 +0000

    Thu, Aug 3, 2023, 11:32 AM +04:30

--- CS50x/PSET3/plurality/plurality.c
+++ CS50x/PSET3/plurality/plurality.c
@@ -81,6 +81,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+    
     for (int i = 0; i <= candidate_count - 1; i++)
     {
         for (int j = 0; j < candidate_count - i -1, j++ )
