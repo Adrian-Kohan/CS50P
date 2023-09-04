ddd00f76163ce88cad9f46bd21acb4bdc4914845
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-04 08:56:00 +0000

    Mon, Sep 4, 2023, 1:26 PM +04:30

--- CS50x/PSET7/movies/12.sql
+++ CS50x/PSET7/movies/12.sql
@@ -1,1 +1,1 @@
-SELECT title FROM movies, people, stars WHERE people.id = stars.person_id AND stars.movie_id = movies.id INTERSECT stars.movie_id = stars.movie_id AND people.name = 'Bradley Cooper' AND people.name = 'Jennifer Lawrence';
+SELECT title FROM movies, people, stars WHERE people.id = stars.person_id AND stars.movie_id = movies.id AND people.name = 'Bradley Cooper' INTERSECT AND people.name = 'Jennifer Lawrence';
