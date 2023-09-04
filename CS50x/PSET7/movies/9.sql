528883b31f54b7c9a08689811c054e129163180f
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-04 08:09:07 +0000

    Mon, Sep 4, 2023, 12:39 PM +04:30

--- CS50x/PSET7/movies/9.sql
+++ CS50x/PSET7/movies/9.sql
@@ -1,1 +1,1 @@
-SELECT name FROM people, stars, movies WHERE people.id = stars.person_id AND stars.movie_id = movies.id AND movies.year = 2004 ORDER BY people.birth HAVING COUNT(peaple.id);
+SELECT DISTINCT name FROM people, stars, movies WHERE people.id = stars.person_id AND stars.movie_id = movies.id AND movies.year = 2004 ORDER BY people.birth;
