0711bbe3b9f91997a8afc9f4247ddf6cc02d259d
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-04 09:00:22 +0000

    Mon, Sep 4, 2023, 1:30 PM +04:30

--- CS50x/PSET7/movies/13.sql
+++ CS50x/PSET7/movies/13.sql
@@ -1,1 +1,1 @@
-SELECT name FROM movies, people, stars WHERE people.id = stars.person_id AND stars.movie_id = movies.id AND people.name = 'Kevin Bacon';
+SELECT name FROM movies, people, stars WHERE people.id = stars.person_id AND stars.movie_id = movies.id AND people.name = 'Kevin Bacon' AND people.birth = 1958;
