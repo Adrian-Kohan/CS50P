c95934195a8cc6245d7fd72edc671f1e881f5858
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-04 08:22:52 +0000

    Mon, Sep 4, 2023, 12:52 PM +04:30

--- CS50x/PSET7/movies/11.sql
+++ CS50x/PSET7/movies/11.sql
@@ -1,1 +1,1 @@
-SELECT title, name, rating FROM movies, people, stars, ratings WHERE people.id = stars.person_id = 'Chadwick Boseman' AND stars.movie_id = ratings.movie_id = movies.id ORDER BY ratings.rating DESC LIMIT 5;
+SELECT title, name, rating FROM movies, people, stars, ratings WHERE people.id = stars.person_id AND stars.movie_id = ratings.movie_id = movies.id AND people.name = 'Chadwick Boseman' ORDER BY ratings.rating DESC LIMIT 5;
