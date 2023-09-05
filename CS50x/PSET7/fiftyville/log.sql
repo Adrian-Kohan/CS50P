0a91fc170535ff18a71f2536683c0ba4d6dfd394
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-05 07:25:07 +0000

    Tue, Sep 5, 2023, 11:55 AM +04:30

--- CS50x/PSET7/fiftyville/log.sql
+++ CS50x/PSET7/fiftyville/log.sql
@@ -1,3 +1,5 @@
 -- Find out where and what time exactly the crime happened (bakery, 10:15am)
 SELECT * FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
+-- Find out the name and id of origin and destination airport  (8, CSF and )
 SELECT * FROM airports;
+SELECT * FROM flights WHERE origin_airport_id = 8 AND day = 28 AND hour >= 10 AND minute > 15;
