0021b8153f8428532df2678ba1374bf2fb6d140b
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-05 07:42:18 +0000

    Tue, Sep 5, 2023, 12:12 PM +04:30

--- CS50x/PSET7/fiftyville/log.sql
+++ CS50x/PSET7/fiftyville/log.sql
@@ -5,4 +5,4 @@ SELECT * FROM airports;
 -- Find out the destination of fiftville flights in 28 July after 10:15 (7, 5, 4 means Dubai, Dallas and New York City)
 SELECT * FROM flights WHERE origin_airport_id = 8 AND day = 28 AND hour >= 10 AND minute > 15;
 SELECT * FROM bakery_security_logs WHERE day = 28 AND hour = 10 AND minute <= 20;
-SELECT * FROM interviews WHERE month = 7 AND day = 28;
+SELECT * FROM interviews WHERE month = 7 AND day = 28 AND trans;
