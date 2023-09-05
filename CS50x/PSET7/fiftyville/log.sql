b0d39e99c8d58c186fd22dea18932f733d22e772
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-05 07:18:43 +0000

    Tue, Sep 5, 2023, 11:48 AM +04:30

--- CS50x/PSET7/fiftyville/log.sql
+++ CS50x/PSET7/fiftyville/log.sql
@@ -1,2 +1,3 @@
 -- Find out where and what time exactly the crime happened (bakery, 10:15am)
 SELECT * FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
+SELECT * FROM airports;
