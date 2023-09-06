4475d46a8b56a7a95f09bfdddba0980a8501001d
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-06 08:37:45 +0000

    Wed, Sep 6, 2023, 1:07 PM +04:30

--- CS50x/PSET6/world-cup/tournament.py
+++ CS50x/PSET6/world-cup/tournament.py
@@ -5,7 +5,7 @@ import sys
 import random
 
 # Number of simluations to run
-N = 1000
+N = 1000000
 
 
 def main():
@@ -62,7 +62,7 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    round = len(team)
+    round = len(teams)
     if round >= 2:
         teams = simulate_round(teams)
         return simulate_tournament(teams)
