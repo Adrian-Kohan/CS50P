8d8918cedf1b4ac5e852e9919023f638ca95dbcc
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-06 07:20:31 +0000

    Wed, Sep 6, 2023, 11:50 AM +04:30

--- CS50x/PSET5/speller/dictionary.c
+++ CS50x/PSET5/speller/dictionary.c
@@ -44,6 +44,20 @@ bool load(const char *dictionary)
 {
     // TODO
     FILE *dict_point = fopen(dictionary, 'r')
+    if (dictionary == NULL)
+    {
+        printf ("Can't open the %\n", dictionary);
+        return false;
+    }
+    char next_word[LENGTH + 1];
+    while(fscanf(dict_point, "%", next_word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return false;
+        }
+    }
     return false;
 }
 
