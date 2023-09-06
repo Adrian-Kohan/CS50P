ae74cfd7085d469267631d1f55048cba65e7c61e
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-06 09:44:09 +0000

    Wed, Sep 6, 2023, 2:14 PM +04:30

--- CS50x/PSET6/dna/dna.py
+++ CS50x/PSET6/dna/dna.py
@@ -3,7 +3,6 @@ import sys
 
 
 def main():
-
     # Check for command-line usage
     if len(sys.argv) < 3:
         print("Usage: python dna.py data.csv sequence.txt")
@@ -30,7 +29,7 @@ def main():
     for person in database:
         match = 0
         for sub in subsequences:
-            if int (person[subsequences]) == result[subsequences]:
+            if int(person[sub]) == result[sub]:
                 match += 1
         if match == len(subsequences):
             print(person["name"])
@@ -49,7 +48,6 @@ def longest_match(sequence, subsequence):
 
     # Check each character in sequence for most consecutive runs of subsequence
     for i in range(sequence_length):
-
         # Initialize count of consecutive runs
         count = 0
 
@@ -57,7 +55,6 @@ def longest_match(sequence, subsequence):
         # If a match, move substring to next potential match in sequence
         # Continue moving substring and checking for matches until out of consecutive matches
         while True:
-
             # Adjust substring start and end
             start = i + count * subsequence_length
             end = start + subsequence_length
