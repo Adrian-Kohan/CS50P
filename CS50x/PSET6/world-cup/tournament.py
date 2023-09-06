3167be948ce9565869304020c13e817493aa5547
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-06 08:23:40 +0000

    Wed, Sep 6, 2023, 12:53 PM +04:30

--- CS50x/PSET6/world-cup/tournament.py
+++ CS50x/PSET6/world-cup/tournament.py
@@ -25,7 +25,11 @@ def main():
             teams.append(team)
 
     counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
+    # Simulate N tournaments and keep track of win counts
+    for simulation in range(N):
+        winner = simulate_tournament(teams)
+        if winner in counts:
+            
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
