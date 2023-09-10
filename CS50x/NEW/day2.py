b08ca61b8dd5680ebab07ce10a72d73fa410e10c
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-10 06:36:38 +0000

    Sun, Sep 10, 2023, 11:06 AM +04:30

---
+++ CS50x/NEW/day2.py
@@ -0,0 +1,6 @@
+age = print(input("What is your current age?"))
+remain_age = 90 - int(age)
+days = remain_age * 365
+weeks = remain_age * 52
+months = remain_age * 12
+print(f"You have {days} days, {} weeks and {} months left")
