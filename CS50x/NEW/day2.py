03374f001c9f3908b7e8ea8bc586eda8569dd730
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-10 06:37:37 +0000

    Sun, Sep 10, 2023, 11:07 AM +04:30

--- CS50x/NEW/day2.py
+++ CS50x/NEW/day2.py
@@ -3,4 +3,4 @@ remain_age = 90 - int(age)
 days = remain_age * 365
 weeks = remain_age * 52
 months = remain_age * 12
-print(f"You have {days} days, {} weeks and {} months left")
+print(f"You have {days} days, {weeks} weeks and {months} months left")
