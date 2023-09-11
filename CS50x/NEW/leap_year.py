5acb5e187bc77093b3902d097913b4bda9de81ae
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-11 07:41:56 +0000

    Mon, Sep 11, 2023, 12:11 PM +04:30

--- CS50x/NEW/leap_year.py
+++ CS50x/NEW/leap_year.py
@@ -1,10 +1,8 @@
 year = int(input("Enter a year. "))
-if year % 4 == 0:
-    print("It's a leap year")
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("It's a leap year")
-    else:
-        print("It's not a leap year")
+if year % 100 == 0:
+    if year % 400 == 0:
+        print("It's a leap year")
+elif year % 4 == 0:
+     print("It's a leap year")
 else:
     print("It's not a leap year")
