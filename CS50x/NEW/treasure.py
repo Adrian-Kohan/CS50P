75b396f5dd86a9057ff11b7f6064d006d828c0eb
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-11 16:02:06 +0000

    Mon, Sep 11, 2023, 8:32 PM +04:30

--- CS50x/NEW/treasure.py
+++ CS50x/NEW/treasure.py
@@ -25,9 +25,9 @@ print("Your mission is to find the treasure.")
 direction = input(print("You're at a cross road. Where do you want to go? Type 'left' or 'right'\n"))
 lower_direction = direction.lower()
 if lower_direction == "left":
-    print
+    print("")
 
 
- left
+ 
 You come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. wait
 You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?
