3c01c9e601f46a1aaf95b483815ec01f671b9e46
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-11 08:27:45 +0000

    Mon, Sep 11, 2023, 12:57 PM +04:30

--- CS50x/NEW/pizza_bill.py
+++ CS50x/NEW/pizza_bill.py
@@ -8,13 +8,10 @@ extra_cheese = input("Do you want extra cheese? Y or N \n")
 bill = 0
 if size == "S":
     bill = 15
-    print("small pizza costs $15")
 elif size == "M":
     bill = 20
-    print("small pizza costs $20")
 else:
     bill = 25
-    print("small pizza costs $25")
 
 if add_pepperoni == "Y":
     if size == "S":
