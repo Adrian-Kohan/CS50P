22b3abed65a976f5e8474d507d6d59e02c79084d
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-11 06:58:01 +0000

    Mon, Sep 11, 2023, 11:28 AM +04:30

--- CS50x/NEW/BMI.py
+++ CS50x/NEW/BMI.py
@@ -1,5 +1,13 @@
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
-BMI= weight / hieght**2
+BMI= weight / height**2
 if BMI < 18.5:
-    print()
+    print("You are underwieght")
+elif 18.5 < BMI < 25:
+    print("You have a normal weight")
+elif 25 < BMI < 30:
+    print("You are overwieght")
+elif 30 < BMI < 35:
+    print("You are obse")
+else:
+    print("You are clinically obse")
