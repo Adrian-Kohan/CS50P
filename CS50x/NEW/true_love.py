5294e7b5464023a15b1ec150f2e1a71832d34739
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-11 15:03:01 +0000

    Mon, Sep 11, 2023, 7:33 PM +04:30

--- CS50x/NEW/true_love.py
+++ CS50x/NEW/true_love.py
@@ -1,6 +1,18 @@
-# Don't change the code below 4
+# Don't change the code below
 print("Welcome to the Love Calculator!")
-namel = input("What is your name? In")
-name2 = input(What is their name? \n")
+name1 = input("What is your name? In")
+name2 = input("What is their name? \n")
 # Don't change the code above
 # Write your code below this line
+lower_name1 = name1.lower()
+lower_name2 = name2.lower()
+T = lower_name1.count("t") + lower_name2.count("t")
+R = lower_name1.count("r") + lower_name2.count("r")
+U = lower_name1.count("u") + lower_name2.count("u")
+E = lower_name1.count("e") + lower_name2.count("e")
+total_true
+L = lower_name1.count("l") + lower_name2.count("l")
+O = lower_name1.count("o") + lower_name2.count("o")
+V = lower_name1.count("v") + lower_name2.count("v")
+E = lower_name1.count("e") + lower_name2.count("e")
+
