7d77eb91883d61085e26b033afdd504eaf1802ea
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-11 14:56:18 +0000

    Mon, Sep 11, 2023, 7:26 PM +04:30

---
+++ CS50x/NEW/true_love.py
@@ -0,0 +1,6 @@
+# Don't change the code below 4
+print("Welcome to the Love Calculator!")
+namel = input("What is your name? In")
+name2 = input(What is their name? \n")
+# Don't change the code above
+# Write your code below this line
