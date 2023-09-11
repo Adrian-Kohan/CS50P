c84323a8a4d30c3ced57184f17c70ec9ce6caaae
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-11 16:27:55 +0000

    Mon, Sep 11, 2023, 8:57 PM +04:30

--- CS50x/NEW/treasure.py
+++ CS50x/NEW/treasure.py
@@ -25,7 +25,7 @@ print(r'''
       ''')
 print("Welcome to Tresure Island.")
 print("Your mission is to find the treasure.")
-direction = input("You're at a cross road. Where do you want to go? Type 'left' or 'right'\n")
+direction = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"\n')
 lower_direction = direction.lower()
 if lower_direction == "left":
     swim_or_boat = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n")
