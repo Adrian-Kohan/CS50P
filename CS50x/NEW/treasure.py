87fa56b5bd5ef46e597fb01539a9c2b8f2a797e5
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-11 16:18:04 +0000

    Mon, Sep 11, 2023, 8:48 PM +04:30

--- CS50x/NEW/treasure.py
+++ CS50x/NEW/treasure.py
@@ -25,13 +25,13 @@ print(r'''
       ''')
 print("Welcome to Tresure Island.")
 print("Your mission is to find the treasure.")
-direction = input(print("You're at a cross road. Where do you want to go? Type 'left' or 'right'\n"))
+direction = input("You're at a cross road. Where do you want to go? Type 'left' or 'right'\n")
 lower_direction = direction.lower()
 if lower_direction == "left":
-    swim_or_boat = input(print("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n"))
+    swim_or_boat = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n")
     lower_swim_or_boat = swim_or_boat.lower()
     if lower_swim_or_boat == "boat":
-        door = input(print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n"))
+        door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
         lower_door = door.lower()
         if lower_door == "yellow":
             print("You win! :)")
