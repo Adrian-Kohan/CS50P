47e4aa3cea05b09ac39447f72dbba7350a2bb0b8
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-11 07:01:12 +0000

    Mon, Sep 11, 2023, 11:31 AM +04:30

--- CS50x/NEW/BMI.py
+++ CS50x/NEW/BMI.py
@@ -1,6 +1,6 @@
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
-BMI= weight / height**2
+BMI= round(weight / height ** 2)
 if BMI < 18.5:
     print("You are underwieght")
 elif 18.5 < BMI < 25:
