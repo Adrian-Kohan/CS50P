1b372760f3d9389912f218fdd23d6fb853dc9677
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-12 07:06:55 +0000

    Tue, Sep 12, 2023, 11:36 AM +04:30

--- CS50x/100 Days/Day4/name.py
+++ CS50x/100 Days/Day4/name.py
@@ -1,5 +1,5 @@
 import random
 names_srting = input("Give me everybody's name, seperated by a comma. ")
 names = names_srting.split(", ")
-random_index = random.randint(0, len(names))
+random_index = random.randint(0, len(names) - 1)
 print(names[random_index])
