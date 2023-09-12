fde24dba6f236464b0e0c74b32dc4ddabde4e6d4
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-12 11:38:39 +0000

    Tue, Sep 12, 2023, 4:08 PM +04:30

--- CS50x/100 Days/Day4/treasure_map.py
+++ CS50x/100 Days/Day4/treasure_map.py
@@ -7,11 +7,12 @@ print(f"{row1}\n{row2}\n{row3}")
 
 position = input("Where do you want to put the treasure?\n")
 vertical = int(position[0]) - 1
-if position[1] == 1:
+horizental = int(position[1])
+if horizental == 1:
     row1[vertical] = "🟨"
-elif position[1] == 2:
+elif horizental == 2:
     row2[vertical] = "🟨"
-else:
+elif horizental == 3:
     row3[vertical] = "🟨"
 
 print(f"{row1}\n{row2}\n{row3}")
