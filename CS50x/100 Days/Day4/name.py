3ad6cfaed542a58ac4e8ea7fd7becf295cce0643
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-12 07:00:31 +0000

    Tue, Sep 12, 2023, 11:30 AM +04:30

--- CS50x/100 Days/Day4/name.py
+++ CS50x/100 Days/Day4/name.py
@@ -1,1 +1,4 @@
-names_srting = 
+names_srting = input("Give me everybody's name, seperated by a comma. ")
+names = names_srting.split(", ")
+random_name = random.randint(0, len(names))
+print(random_name)
