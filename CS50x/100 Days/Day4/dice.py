72db91e989e4cb3c74fe73ecbc6583cd9ef46539
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-12 06:05:30 +0000

    Tue, Sep 12, 2023, 10:35 AM +04:30

--- CS50x/100 Days/Day4/dice.py
+++ CS50x/100 Days/Day4/dice.py
@@ -1,1 +1,14 @@
 import random
+print(r'''
+  _______
+  /\ o o o\
+ /o \ o o o\_______
+<    >------>   o /|
+ \ o/  o   /_____/o|
+  \/______/     |oo|
+        |   o   |o/
+        |_______|/
+''')
+
+dice = random.randint(1,6)
+print(dice)
