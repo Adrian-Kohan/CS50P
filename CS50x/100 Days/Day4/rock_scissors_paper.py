087c61b3fedcf9557d3c636a6321fd313e73ec20
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-12 12:11:30 +0000

    Tue, Sep 12, 2023, 4:41 PM +04:30

--- CS50x/100 Days/Day4/rock_scissors_paper.py
+++ CS50x/100 Days/Day4/rock_scissors_paper.py
@@ -1,3 +1,4 @@
+import random
 rock = """
     _______
 ---'   ____)
@@ -27,3 +28,22 @@ scissors = """
 
 shapes = [rock, paper, scissors]
 user = input("What do you choose?Type 0 for Rock, 1 for Paper and 2 for Scissors.\n")
+print(shapes[user])
+print("Computer chose:")
+computer = random.choice(shapes)
+print(computer)
+
+if computer == "rock" and shapes[user] == "scissors":
+    priint("You lose")
+elif computer == "rock" and shapes[user] == "rock":
+    priint("Tie")
+elif computer == "paper" and shapes[user] == "rock":
+    priint("You lose")
+elif computer == "paper" and shapes[user] == "paper":
+    priint("Tie")
+elif computer == "scissors" and shapes[user] == "rock":
+    priint("You lose")
+elif computer == "scissors" and shapes[user] == "scissors":
+    priint("Tie")
+else:
+    print("You win")
