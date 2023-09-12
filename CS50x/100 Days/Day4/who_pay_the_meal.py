887a9d21065d3d6750b998b3077568647373b6e4
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-12 07:20:57 +0000

    Tue, Sep 12, 2023, 11:50 AM +04:30

--- CS50x/100 Days/Day4/who_pay_the_meal.py
+++ CS50x/100 Days/Day4/who_pay_the_meal.py
@@ -2,6 +2,6 @@ import random
 names_srting = input("Give me everybody's name, seperated by a comma. ")
 names = names_srting.split(", ")
 random_index = random.randint(0, len(names) - 1)
-print(names[random_index] + "Have to pay the bill")
+print(names[random_index] + "Have to pay the meal")
 
 # or simply we can write random.choice(names)
