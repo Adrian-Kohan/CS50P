21532e054c1511e587fd76542154daa817bd8688
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-12 11:30:59 +0000

    Tue, Sep 12, 2023, 4:00 PM +04:30

--- CS50x/100 Days/Day4/treasure_map.py
+++ CS50x/100 Days/Day4/treasure_map.py
@@ -5,8 +5,16 @@ row3 = ["⬜️", "⬜️", "⬜️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 
-position = input("Where do you want to put the treasure?")
+position = input("Where do you want to put the treasure?\n")
 
-if position[0] == row1[0 - 1]:
-    
+if position[1] == 1:
+    row1[posision[0] - 1] = "X"
+elif position[1] == 2:
+    row2[posision[0] - 1] = "X"
+else:
+    row3[posision[0] - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
+
+
 
