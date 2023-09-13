2da3f43509b35c64bc76f14939d0c41e25e13e99
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-13 07:33:22 +0000

    Wed, Sep 13, 2023, 12:03 PM +04:30

--- CS50x/100 Days/Day5/fizzbuzz.py
+++ CS50x/100 Days/Day5/fizzbuzz.py
@@ -3,7 +3,6 @@ for i in range(1, 100):
         print("Fizz")
     elif i % 5 == 0:
         print("Buzz")
-    elif i % 3 == 0 and i % 5 == 0:
-        print("Fizzbuzz")
+    
     else:
         print(i)
