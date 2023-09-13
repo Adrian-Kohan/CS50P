f6f8b7c078419c2fc94417caea9c087471d48510
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-13 07:50:46 +0000

    Wed, Sep 13, 2023, 12:20 PM +04:30

--- CS50x/100 Days/Day5/PyPassword_generator.py
+++ CS50x/100 Days/Day5/PyPassword_generator.py
@@ -10,3 +10,6 @@ nr_letters = int (input ("How many lettêrs would you like in your password?\n")
 nr_symbols = int(input (f"How many symbols would you like?\n"))
 
 nr_numbers = int(input (f"How many numbers would you like?\n"))
+
+password = 0
+for 
