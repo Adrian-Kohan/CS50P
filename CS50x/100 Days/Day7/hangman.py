38931d813efca638b1b2568a75d9dd7d030020ee
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-17 11:01:17 +0000

    Sun, Sep 17, 2023, 3:31 PM +04:30

--- CS50x/100 Days/Day7/hangman.py
+++ CS50x/100 Days/Day7/hangman.py
@@ -4,13 +4,14 @@ word_list = ["ardvark", "baboon", "camel"]
 #randomly chosen a word from word_list and assign it to a variable named word_chosen
 chosen_word = random.choice(word_list)
 print(chosen_word)
-#ask the user to guess a letter and assign it to a variable called guess. make guess lowercase
-
-guess = input("Guess a letter: ").lower()
 guess_list = []
 for i in chosen_word:
     guess_list.append("_")
     #or guess_list += "_"
+#ask the user to guess a letter and assign it to a variable called guess. make guess lowercase
+while i in guess_list 
+guess = input("Guess a letter: ").lower()
+
 
 #check if the letter user guessed is one of the lettere in the chosen word
 for i in range(len(chosen_word)):
