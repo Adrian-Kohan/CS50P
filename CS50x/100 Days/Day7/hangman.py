11e3afa93d78f178cf09313975bb497552a0dba4
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-17 11:49:18 +0000

    Sun, Sep 17, 2023, 4:19 PM +04:30

--- CS50x/100 Days/Day7/hangman.py
+++ CS50x/100 Days/Day7/hangman.py
@@ -82,8 +82,7 @@ for i in chosen_word:
     guess_list.append("_")
     #or guess_list += "_"
 #ask the user to guess a letter and assign it to a variable called guess. make guess lowercase
-end_game = False
-while not end_game:
+while "_" in guess_list:
     guess = input("Guess a letter: ").lower()
     #check if the letter user guessed is one of the lettere in the chosen word
     s = 0
@@ -91,13 +90,12 @@ while not end_game:
         if guess == chosen_word[i]:
             guess_list[i] = guess
             print(guess_list)
-        else:
-            lives -= 1
+    if guess != chosen_word[i] and lives != 0:
+        lives -= 1
         print(stages[s])
         s += 1
-    if lives == 0:
+    elif lives == 0:
         print("You loose.")
     elif "_" not in guess_list:
-        end_game = True
         print("You win.")
 
