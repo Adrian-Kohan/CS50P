1b66ad18a981302352572f1662f52fb9851e9c65
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-18 09:21:14 +0000

    Mon, Sep 18, 2023, 12:51 PM +03:30

--- CS50x/100 Days/Day8/prime_number.py
+++ CS50x/100 Days/Day8/prime_number.py
@@ -1,6 +1,6 @@
 n = int(input("Check this number: "))
 def prime_checker(number):
-    if number % 2 == 0 or number % 3 == 0 or number % 5 == 0 or number % 7 == 0
+    if number % 2 == 0 or number % 3 == 0 or number % 5 == 0 or number == number * number:
         print("It's not a prime number")
     else:
         print("It's a prime number")
