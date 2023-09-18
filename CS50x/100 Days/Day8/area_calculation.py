5acc0dbf315c04d6d990c8cdb662c020bc441f6d
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-18 06:30:02 +0000

    Mon, Sep 18, 2023, 11:00 AM +04:30

---
+++ CS50x/100 Days/Day8/area_calculation.py
@@ -0,0 +1,8 @@
+w = int(input("Width of wall: "))
+h = int(input("Heigth of wall: "))
+cover  = 5
+def area_calculation(w, h, cover):
+    print("Hello")
+    print("world")
+    print("!")
+greet()
