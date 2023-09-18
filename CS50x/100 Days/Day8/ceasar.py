7720ffe772ca4d9b7762dbc557588a815ce75ec4
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-18 15:22:47 +0000

    Mon, Sep 18, 2023, 6:52 PM +03:30

--- CS50x/100 Days/Day8/ceasar.py
+++ CS50x/100 Days/Day8/ceasar.py
@@ -2,49 +2,6 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input ("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
 text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number: \n"))
-def encrypt(sh, txt):
-    #create a new list that holds new encrypted alphabet
-    #we just can duplicate our alphabet list [a-z,a-z] instead of doing the below
-    new_alphabet = []
-    for i in range(len(alphabet)):
-        if (i + shift) < len(alphabet):
-            new_alphabet += alphabet[i + shift]
-        else:
-            start = (i + shift) - len(alphabet)
-            new_alphabet += alphabet[start]
-
-    #encode new text based of the alphabet position with new alphabet list
-    #and we can just use alphabet.index(ch) instead of some of the below code
-    new_txt = ""
-    for ch in txt:
-        for i in range(len(new_alphabet)):
-            if alphabet[i] == ch:
-                new_txt += new_alphabet[i]
-
-    print(f"The encoded text is: {new_txt}")
-
-def decrypt(sh, txt):
-    #create a new list that holds new encrypted alphabet
-    #we just can duplicate our alphabet list [a-z,a-z] instead of doing the below
-    new_alphabet = []
-    for i in range(len(alphabet)):
-        if (i + shift) < len(alphabet):
-            new_alphabet += alphabet[i + shift]
-        else:
-            start = (i + shift) - len(alphabet)
-            new_alphabet += alphabet[start]
-
-    #decode new text based of the new alphabet position with alphabet list
-    new_txt = ""
-    for ch in txt:
-        new_txt += alphabet[new_alphabet.index(ch)]
-
-
-    print(f"The decoded text is: {new_txt}")
-
-
-
-
 def caesar(sh, txt, direc):
     #create a new list that holds new encrypted alphabet
     #we just can duplicate our alphabet list [a-z,a-z] instead of doing the below
@@ -57,9 +14,22 @@ def caesar(sh, txt, direc):
             new_alphabet += alphabet[start]
 
     if direction == "encode":
+        new_txt = ""
+    for ch in txt:
+        for i in range(len(new_alphabet)):
+            if alphabet[i] == ch:
+                new_txt += new_alphabet[i]
+
+    print(f"The encoded text is: {new_txt}")
 
     elif direction == "decode":
-        decrypt(sh = shift, txt = text)
+         #decode new text based of the new alphabet position with alphabet list
+    new_txt = ""
+    for ch in txt:
+        new_txt += alphabet[new_alphabet.index(ch)]
+
+
+    print(f"The decoded text is: {new_txt}")
     else:
         print("Please insert correct input")
 
