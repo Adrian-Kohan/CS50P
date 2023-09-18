cbb19f38516417c55991b2ba6dc8611ffa1d71c4
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-18 10:03:04 +0000

    Mon, Sep 18, 2023, 1:33 PM +03:30

--- CS50x/100 Days/Day8/ceasar.py
+++ CS50x/100 Days/Day8/ceasar.py
@@ -4,3 +4,4 @@ direction = input ("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
 text = input("Type your message: \n").Lower()
 5
 shift = int(input("Type the shift number: \n"))
+def encrypt(txt, shift)
