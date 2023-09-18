3d51b38454242a196ca8cfe8c4111d2bb5dc38ee
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-18 11:14:51 +0000

    Mon, Sep 18, 2023, 2:44 PM +03:30

--- CS50x/100 Days/Day8/ceasar.py
+++ CS50x/100 Days/Day8/ceasar.py
@@ -1,15 +1,14 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'z', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 direction = input ("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
 text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number: \n"))
 def encrypt(sh):
-    new =[]
     for i in range(len(alphabet)):
-        if (i + shift) <= len(alphabet):
-            new += alphabet[i + shift]
+        if (i + shift) < len(alphabet):
+            alphabet[i] = alphabet[i + shift]
         else:
             start = (i + shift) - len(alphabet)
-            new += alphabet[start]
-    print(new)
+            alphabet[i] = alphabet[start]
+    print(alphabet)
 
 encrypt(shift)
