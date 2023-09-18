a868b18129aa72fe9169a7ed1e345867c720a2bb
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-18 10:57:44 +0000

    Mon, Sep 18, 2023, 2:27 PM +03:30

--- CS50x/100 Days/Day8/ceasar.py
+++ CS50x/100 Days/Day8/ceasar.py
@@ -1,8 +1,6 @@
 alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'z', 'y', 'z']
 direction = input ("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
-4
-text = input("Type your message: \n").Lower()
-5
+text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number: \n"))
 def encrypt(shift):
     for i in range(len(alphabet)):
