991a27a547de9888cea66ace4d3146247e5907ec
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-18 10:01:55 +0000

    Mon, Sep 18, 2023, 1:31 PM +03:30

---
+++ CS50x/100 Days/Day8/ceasar.py
@@ -0,0 +1,6 @@
+alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'z', 'y', 'z']
+direction = input ("Type 'encode' to encrypt, type 'decode' to decrypt: \n")
+4
+text = input("Type your message: \n").Lower()
+5
+shift = int(input("Type the shift number: \n"))
