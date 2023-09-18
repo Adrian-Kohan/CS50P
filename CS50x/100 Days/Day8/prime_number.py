1388ebe6ca01765d8a64d3801461ef9fac5a1860
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-18 09:34:58 +0000

    Mon, Sep 18, 2023, 1:04 PM +03:30

--- CS50x/100 Days/Day8/prime_number.py
+++ CS50x/100 Days/Day8/prime_number.py
@@ -1,15 +1,13 @@
 n = int(input("Check this number: "))
 def prime_checker(number):
     for i in range(2,number):
-        num = ""
         if number % i == 0 and i != number:
             num = "not a prime"
     print(f"It's {num} number")
 
     for i in range(2,number):
-        num = ""
         if number % i != 0:
             num = "a prime"
-        print(f"It's {num} number")
-        
+    print(f"It's {num} number")
+
 prime_checker(number = n)
