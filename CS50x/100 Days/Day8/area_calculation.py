a7233c9ca15e9f0ef46929d1f2245c632ddee52a
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-18 06:34:49 +0000

    Mon, Sep 18, 2023, 11:04 AM +04:30

--- CS50x/100 Days/Day8/area_calculation.py
+++ CS50x/100 Days/Day8/area_calculation.py
@@ -3,5 +3,6 @@ h = int(input("Heigth of wall: "))
 cover  = 5
 def area_calculation(w, h, cover):
     number_of_cans = round((w * h) / cover)
+    print(f"You'll need {number_of_cans} cans of paint")
 
-are_calculation()
+area_calculation(5,5,5)
