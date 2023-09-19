6f2ebd3878451a7fa42c0c6aafd9e7bbde3728a0
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-19 08:21:30 +0000

    Tue, Sep 19, 2023, 11:51 AM +03:30

---
+++ CS50x/100 Days/Day9/blind_auction.py
@@ -0,0 +1,8 @@
+from gavel import logo
+print(logo)
+print("Welcome to the secret auction program.")
+name = input("What is your name?: ")
+bid = input("What's your bid? $")
+
+def auction(input_name, input_bid):
+    
