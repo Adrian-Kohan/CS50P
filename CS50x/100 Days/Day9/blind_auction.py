5968420975352e5387a87f70677434e4f56d3e31
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-19 17:33:50 +0000

    Tue, Sep 19, 2023, 9:03 PM +03:30

--- CS50x/100 Days/Day9/blind_auction.py
+++ CS50x/100 Days/Day9/blind_auction.py
@@ -2,29 +2,30 @@ from gavel import logo
 from replit import clear
 print(logo)
 print("Welcome to the secret auction program.")
-name = input("What is your name?: ")
-bid = input("What's your bid? $")
-
-def auction(input_name, input_bid):
-    secret_auction = []
-    secret_auction += {name : bid}
-    while True:
-        answer = input("Are there any other bidders? Type 'yes' or 'no'\n")
-        if answer == "yes":
-            clear()
-            name = input("What is your name?: ")
-            bid = input("What's your bid? $")
-            continue
-
-        elif answer == "no":
-            auction(name, bid)
-            break
+def auction(input_name):
     max = secret_auction[0][0]
     for i in range(len(secret_auction)):
         if secret_auction[0][0] < secret_auction[i][0]:
             max = secret_auction[i][0]
     print(secret_auction)
-    print(f"The winner is {name} with a bid of {max}")
+    print(f"The winner is {input_name} with a bid of {max}")
 
+while True:
+    name = input("What is your name?: ")
+    bid = input("What's your bid? $")
+    secret_auction = []
+    secret_auction += {name : bid}
+    answer = input("Are there any other bidders? Type 'yes' or 'no'\n")
+    if answer == "yes":
+        clear()
+        name = input("What is your name?: ")
+        bid = input("What's your bid? $")
+        secret_auction = []
+        secret_auction += {name : bid}
+        continue
+
+    elif answer == "no":
+        auction()
+        break
 
 
