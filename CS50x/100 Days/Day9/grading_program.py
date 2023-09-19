60a6b9d1f787220ebd3c15dbd591809d58548916
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-19 06:31:32 +0000

    Tue, Sep 19, 2023, 10:01 AM +03:30

---
+++ CS50x/100 Days/Day9/grading_program.py
@@ -0,0 +1,11 @@
+student_score = {
+    "Harry" : 81,
+    "Ron" : 78,
+    "Hermione" : 99,
+    "Draco" :74,
+    "Neville" : 62
+}
+
+student_grade = {}
+for key in student_score:
+    if student_score[key] > 
