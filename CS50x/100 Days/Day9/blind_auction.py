773b925a2dea2b8b798afbe4e6709c59d141f901
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-19 18:01:46 +0000

    Tue, Sep 19, 2023, 9:31 PM +03:30

--- CS50x/100 Days/Day9/blind_auction.py
+++ CS50x/100 Days/Day9/blind_auction.py
@@ -1,6 +1,6 @@
-#from gavel import logo
+from gavel import logo
 from replit import clear
-#print(logo)
+print(logo)
 print("Welcome to the secret auction program.")
 name = input("What is your name?: ")
 bid = input("What's your bid? $")
@@ -9,6 +9,12 @@ secret_auction =[]
 def auction(input_name, input_bid):
     secret_auction.append({input_name : input_bid })
     print(secret_auction)
+    
+    max = secret_auction[0][0]
+    for i in range(len(secret_auction)):
+        if secret_auction[0][0] < secret_auction[i][0]:
+            max = secret_auction[i][0]
+    print(f"The winner is {input_name} with a bid of {max}")
 
 
 auction(name, bid)
--- CS50x/100 Days/Day9/gavel.py
+++ CS50x/100 Days/Day9/gavel.py
@@ -15,8 +15,4 @@ logo = r'''
                       /_______________\
 '''
 
-max = secret_auction[0][0]
-    for i in range(len(secret_auction)):
-        if secret_auction[0][0] < secret_auction[i][0]:
-            max = secret_auction[i][0]
-    print(f"The winner is {input_name} with a bid of {max}")
+
