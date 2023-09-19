b0bd3a64f25fb184009fd3c0b57c5b9ef9041403
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-19 19:02:55 +0000

    Tue, Sep 19, 2023, 10:32 PM +03:30

--- CS50x/100 Days/Day9/blind_auction.py
+++ CS50x/100 Days/Day9/blind_auction.py
@@ -4,16 +4,6 @@ print(logo)
 print("Welcome to the secret auction program.")
 secret_auction = {}
 
-def winner(auction):
-    max = 0
-    winner = ""
-    for person in auction:
-        bid_amount = auction[name]
-        if bid_amount > max:
-            max = bid_amount
-            winner = person
-    print(f"The winner is {winner} with a bid of {max}")
-
 while True:
     name = input("What is your name?: ")
     bid = int(input("What's your bid? $"))
@@ -22,7 +12,13 @@ while True:
     if answer == "yes":
         clear()
         continue
+
     elif answer == "no":
-        winner(secret_auction)
+         max = 0
+        for person in secret_auction:
+            if secret_auction[name] > max:
+                max = bid_amount
+                winner = person
+        print(f"The winner is {winner} with a bid of {max}")
         break
 
