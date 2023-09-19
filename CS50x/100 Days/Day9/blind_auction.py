2e5041f7c1c3fec1cbd5e92cfa652b80b98f1c81
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-19 17:12:24 +0000

    Tue, Sep 19, 2023, 8:42 PM +03:30

--- CS50x/100 Days/Day9/blind_auction.py
+++ CS50x/100 Days/Day9/blind_auction.py
@@ -7,12 +7,12 @@ bid = input("What's your bid? $")
 
 def auction(input_name, input_bid):
     secret_auction = []
-    secret_auction += {name : [bid]}
+    secret_auction += {name : bid}
 
-    max = secret_auction[0][name][0]
+    max = secret_auction[0][0]
     for i in range(len(secret_auction)):
-        if secret_auction[0][name][0] < secret_auction[i][name][0]:
-            max = secret_auction[i][name][0]
+        if secret_auction[0][0] < secret_auction[i][0]:
+            max = secret_auction[i][0]
     print(secret_auction)
     print(f"The winner is {name} with a bid of {max}")
 
