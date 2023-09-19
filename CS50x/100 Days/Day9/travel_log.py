0bf0aef9b0394c4eed51e4653d6128b0cf39b128
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-19 07:19:20 +0000

    Tue, Sep 19, 2023, 10:49 AM +03:30

--- CS50x/100 Days/Day9/travel_log.py
+++ CS50x/100 Days/Day9/travel_log.py
@@ -2,13 +2,19 @@ travel_log = [
 {
 "country": "France",
 "visits": 12,
-"cities": ["Paris", "Lille",
-"Di jon"]
+"cities": ["Paris", "Lille","Di jon"]
+},
 {
 "country": "Germany",
 "visits": 5,
-"cities": ["Berlin",
-. "Hambura'
-"Stuttgart"]
+"cities": ["Berlin", "Hambura", "Stuttgart"]
 },
 ]
+def add_new_country(country, visits, cities):
+    travel_log.append({
+        "country" : country
+        "visits" : visits
+        "cities" : cities
+    })
+    
+add_new_country("Russia, 2, ["Moscow", "Saint Peters"]")
