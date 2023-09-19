bb2e3939ef1cc72441d976df3ddb4c2f7f95dff2
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-19 07:13:16 +0000

    Tue, Sep 19, 2023, 10:43 AM +03:30

---
+++ CS50x/100 Days/Day9/travel_log.py
@@ -0,0 +1,14 @@
+travel_log = [
+{
+"country": "France",
+"visits": 12,
+"cities": ["Paris", "Lille",
+"Di jon"]
+{
+"country": "Germany",
+"visits": 5,
+"cities": ["Berlin",
+. "Hambura'
+"Stuttgart"]
+},
+]
