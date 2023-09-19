0174240967db23a17de195d2f920a743060f5fd2
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-19 18:02:25 +0000

    Tue, Sep 19, 2023, 9:32 PM +03:30

--- CS50x/100 Days/Day9/blind_auction.py
+++ CS50x/100 Days/Day9/blind_auction.py
@@ -7,9 +7,9 @@ bid = input("What's your bid? $")
 secret_auction =[]
 
 def auction(input_name, input_bid):
-    secret_auction.append({input_name : input_bid })
+    secret_auction.append({input_name : [input_bid] })
     print(secret_auction)
-    
+
     max = secret_auction[0][0]
     for i in range(len(secret_auction)):
         if secret_auction[0][0] < secret_auction[i][0]:
