c36bd097876fae615d3a1d8a1cb56521ffa577b4
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-20 08:45:21 +0000

    Wed, Sep 20, 2023, 12:15 PM +03:30

--- CS50x/100 Days/Day10/calculator.py
+++ CS50x/100 Days/Day10/calculator.py
@@ -10,10 +10,6 @@ def divide(n1, n2):
 def subtract(n1, n2):
     return n1 - n2
 
-
-
-num1 = int(input("What's the first number?: "))
-
 operations = {
     "*" : multiply,
     "+" : add,
@@ -21,21 +17,23 @@ operations = {
     "/" : divide
 }
 
-
-for operator in operations:
-    print(operator)
-
-operation_symbol =input("Pick an operation from the line above: ")
-
-num2 = int(input("What's the second number?: "))
-
-for operator in operations:
-    function = operations[operation_symbol]
-    answer = function(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-
 continue_calculating = True
+
 while continue_calculating:
-    anwser2 = input(f"Type 'y' to continue calculating with {answer}, or type 'no' to wxit")
-    if anwser2 == 'y':
+    num1 = int(input("What's the first number?: "))
+    for operator in operations:
+        print(operator)
+
+    operation_symbol =input("Pick an operation from the line above: ")
+    num2 = int(input("What's the second number?: "))
+
+    for operator in operations:
+        function = operations[operation_symbol]
+        answer = function(num1, num2)
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    anwser2 = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")
+
+    if anwser2 == 'n':
+        continue_calculating = False
+
