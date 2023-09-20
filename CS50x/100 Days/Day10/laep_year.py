76f815ada1b805862ed5965babe04b27ed02212d
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-20 07:28:22 +0000

    Wed, Sep 20, 2023, 10:58 AM +03:30

--- CS50x/100 Days/Day10/laep_year.py
+++ CS50x/100 Days/Day10/laep_year.py
@@ -1,14 +1,21 @@
-year = int(input("Enter a year. "))
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("It's a leap year")
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
         else:
-            print("It's not a leap year")
+            return True
     else:
-        print("It's a leap year")
-else:
-    print("It's not a leap year")
+        return False
 
-def days_in_month():
-    month_days = [31, 28, 31, 30, 31, 30, 31, 31, ]
+def days_in_month(input_year, input_month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap_year
+
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
