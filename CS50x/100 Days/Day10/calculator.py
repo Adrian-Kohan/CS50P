b3716b30d9f49665e5abec38248a81307bdf0e19
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-20 08:22:24 +0000

    Wed, Sep 20, 2023, 11:52 AM +03:30

--- CS50x/100 Days/Day10/calculator.py
+++ CS50x/100 Days/Day10/calculator.py
@@ -13,21 +13,24 @@ def subtract(n1, n2):
 
 
 num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
 
 operations = {
-    "*" : multiply(num1, num2),
-    "+" : add(num1, num2),
-    "-" : subtract(num1, num2),
-    "/" : divide(num1, num2)
+    "*" : multiply,
+    "+" : add,
+    "-" : subtract,
+    "/" : divide
 }
+
 
 for operator in operations:
     print(operator)
 
+num2 = int(input("What's the second number?: "))
+
 operation_symbol =input("Pick an operation from the line above: ")
 
 for operator in operations:
-    answer = operations[operation_symbol]
+    function = operations[operation_symbol]
+    answer = function(num1, num2)
 
 print(f"{num1} {operation_symbol} {num2} = {answer}")
