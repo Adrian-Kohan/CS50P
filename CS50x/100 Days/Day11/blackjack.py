51f0f7ffc06581aa9860dc39b4c5821c8c713ae6
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-21 14:01:26 +0000

    Thu, Sep 21, 2023, 5:31 PM +03:30

--- CS50x/100 Days/Day11/blackjack.py
+++ CS50x/100 Days/Day11/blackjack.py
@@ -1,16 +1,3 @@
-# # # # # # # # # # # # # # #  Our Blackjack House Rules # # # # # # # # # # # # # # # # #
-
-# The deck is unlimited in size.
-# There are no jokers.
-# The Jack/Queen/King all count as 10.
-# The the Ace can count as 11 or 1.
-# Use the following list as the deck of cards:
-# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-# The cards in the list have equal probability of being drawn.
-# Cards are not removed from the deck as they are drawn.
-
-# # # # # # # # # # # # # # # # # # # # #  Hints # # # # # # # # # # # # # # # # # # # # #
-
 from art import logo
 from replit import clear
 import random
@@ -18,13 +5,11 @@ import random
 def blackjack():
     print(logo)
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
     your_cards = random.sample(cards, 2)
     current_score = sum(your_cards)
     print(f"Your cards: {your_cards}, Current score: {current_score}")
     computer_first_card = random.choice(cards)
     print(f"Computer's first card: {computer_first_card}")
-
 
     def winner():
         computer_second_card = random.sample(cards, 1)
