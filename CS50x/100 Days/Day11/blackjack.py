924290c6ff2a4765b83faae601844970cb74914a
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-21 04:55:20 +0000

    Thu, Sep 21, 2023, 8:25 AM +03:30

--- CS50x/100 Days/Day11/blackjack.py
+++ CS50x/100 Days/Day11/blackjack.py
@@ -11,3 +11,12 @@
 
 # # # # # # # # # # # # # # # # # # # # #  Hints # # # # # # # # # # # # # # # # # # # # #
 
+from art import logo
+from replit import clear
+
+print(logo)
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
+
