e096d964791020cde91aa1644efcc680ff6845eb
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-21 06:46:15 +0000

    Thu, Sep 21, 2023, 10:16 AM +03:30

--- CS50x/100 Days/Day11/blackjack.py
+++ CS50x/100 Days/Day11/blackjack.py
@@ -32,6 +32,7 @@ while continue_game:
     another_card = input("Type 'y' to get another card, type 'n' to pass: ")
     if another_card == "n":
         continue_game = False
+        
     choice = random.choice(cards)
     your_cards.append(choice)
     current_score += choice
