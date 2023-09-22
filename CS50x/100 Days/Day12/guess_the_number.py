d414fd32b16433c94f9d45be73a2494feb269af9
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-22 09:28:47 +0000

    Fri, Sep 22, 2023, 12:58 PM +03:30

--- CS50x/100 Days/Day12/guess_the_number.py
+++ CS50x/100 Days/Day12/guess_the_number.py
@@ -1,3 +1,22 @@
+import random
+
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
-print("Choose a difficulty. Type 'easy' or 'hard'")
+random_number = random.choice(1, 101)
+user_choice = input("Choose a difficulty. Type 'easy' or 'hard': ")
+if user_choice == "hard":
+    print("You have 5 attempts remaining to guess the number.")
+    number_of_attempts = 5
+elif user_choice == "easy":
+    print("You have 10 attempts remaining to guess the number.")
+    number_of_attempts = 10
+
+def game(number_of_attempts):
+    for i in range(number_of_attempts):
+        guess = int(input("Make a guess: "))
+        if guess > random_number:
+            return "Too high"
+        elif guess < random_number:
+            return "Too low"
+        else:
+            return 
