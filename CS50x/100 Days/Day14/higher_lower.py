0fc02a66ee498c99292a234228eed8307c29e7ef
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-24 10:14:01 +0000

    Sun, Sep 24, 2023, 1:44 PM +03:30

--- CS50x/100 Days/Day14/higher_lower.py
+++ CS50x/100 Days/Day14/higher_lower.py
@@ -2,25 +2,25 @@ from art import logo, vs
 from game_data import data
 from replit import clear
 import random
-print(logo)
 
+print(logo)
 
 def random_A_and_B():
     return random.choice(data)
 
 
-def score_calculator():
+def game():
     score = 0
     while True:
         A = random_A_and_B()
         B = random_A_and_B()
 
-        def game():
+        def comparation():
             print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
             print(vs)
             print(f"Against B: {B['name']}, a {B['description']}, from {B['country']}.")
 
-        game()
+        comparation()
         answer = input("Who has more followers? Type 'A' or 'B': ")
         if answer == 'A':
             if A['follower_count'] > B['follower_count']:
@@ -41,4 +41,4 @@ def score_calculator():
                 print(f"Sorry, that's wrong. Final score: {score}")
                 return
 
-score_calculator()
+game()
