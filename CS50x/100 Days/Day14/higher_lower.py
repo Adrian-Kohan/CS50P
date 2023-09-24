189163230f79a22bb3361f4572cb1215824b6798
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-24 09:43:18 +0000

    Sun, Sep 24, 2023, 1:13 PM +03:30

--- CS50x/100 Days/Day14/higher_lower.py
+++ CS50x/100 Days/Day14/higher_lower.py
@@ -10,6 +10,8 @@ print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
 print(vs)
 print(f"Against B: {B['name']}, a {B['description']}, from {B['country']}.")
 answer = input("Who has more followers? Type 'A' or 'B': ")
+score = 0
 if answer == 'A':
     if A['follower_count'] > B['follower_count']:
-        
+
+        print("Sorry, that's wrong. Final score: {score}")
