e12855950799b2671c0bd378a2c5ab2a29fcc8ef
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-24 09:42:03 +0000

    Sun, Sep 24, 2023, 1:12 PM +03:30

--- CS50x/100 Days/Day14/higher_lower.py
+++ CS50x/100 Days/Day14/higher_lower.py
@@ -2,10 +2,14 @@ from art import logo, vs
 from game_data import data
 import random
 
+A = random.choice(data)
+B = random.choice(data)
+
 print(logo)
-choosen_person = random.choice(data)
-
-print(f"Compare A: {choosen_person[name]}")
-
-
+print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
 print(vs)
+print(f"Against B: {B['name']}, a {B['description']}, from {B['country']}.")
+answer = input("Who has more followers? Type 'A' or 'B': ")
+if answer == 'A':
+    if A['follower_count'] > B['follower_count']:
+        
