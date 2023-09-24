f10faaf5049db24f547d0a895d4b3cf67e60238d
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-24 11:20:15 +0000

    Sun, Sep 24, 2023, 2:50 PM +03:30

--- CS50x/100 Days/Day14/higher_lower.py
+++ CS50x/100 Days/Day14/higher_lower.py
@@ -8,26 +8,26 @@ print(logo)
 def random_A_and_B():
     return random.choice(data)
 
+A = random_A_and_B()
+B = random_A_and_B()
 
-def game():
-    score = 0
-    while True:
-        A = random_A_and_B()
-        B = random_A_and_B()
+def comparation():
+    print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
+    print(vs)
+    print(f"Against B: {B['name']}, a {B['description']}, from {B['country']}.")
 
-        def comparation():
-            print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
-            print(vs)
-            print(f"Against B: {B['name']}, a {B['description']}, from {B['country']}.")
+comparation()
 
-        comparation()
+score = 0
+
+def result():
         answer = input("Who has more followers? Type 'A' or 'B': ")
         if answer == 'A':
             if A['follower_count'] > B['follower_count']:
                 clear()
                 print(logo)
-                score += 1
-                print(f"You're right! Current score: {score}")
+                return score + 1
+                return (f"You're right! Current score: {score}")
             else:
                 print(f"Sorry, that's wrong. Final score: {score}")
                 return
@@ -40,5 +40,10 @@ def game():
             else:
                 print(f"Sorry, that's wrong. Final score: {score}")
                 return
+
+def game():
+    score = 0
+    while True:
+
 
 game()
