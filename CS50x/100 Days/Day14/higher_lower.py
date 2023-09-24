4f2e957cd8b8a0cd5c49d2904a33e2562b062426
Adrian-Kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-09-24 09:53:40 +0000

    Sun, Sep 24, 2023, 1:23 PM +03:30

--- CS50x/100 Days/Day14/higher_lower.py
+++ CS50x/100 Days/Day14/higher_lower.py
@@ -2,31 +2,34 @@ from art import logo, vs
 from game_data import data
 import random
 
-A = random.choice(data)
-B = random.choice(data)
-
 def game():
     print(logo)
     print(f"Compare A: {A['name']}, a {A['description']}, from {A['country']}.")
     print(vs)
     print(f"Against B: {B['name']}, a {B['description']}, from {B['country']}.")
 
-score = 0
-while True:
-    answer = input("Who has more followers? Type 'A' or 'B': ")
-    if answer == 'A':
-        if A['follower_count'] > B['follower_count']:
-            print("You're right! Current score: {score}")
-            score += 1
-            game()
+def score_calculator():
+    score = 0
+    while True:
+        A = random.choice(data)
+        B = random.choice(data)
+        answer = input("Who has more followers? Type 'A' or 'B': ")
+        if answer == 'A':
+            if A['follower_count'] > B['follower_count']:
+                print(f"You're right! Current score: {score}")
+                score += 1
+                game()
+            else:
+                print(f"Sorry, that's wrong. Final score: {score}")
+                return
         else:
-            print("Sorry, that's wrong. Final score: {score}")
-            return
-    else:
-        if A['follower_count'] < B['follower_count']:
-            print("You're right! Current score: {score}")
-            score += 1
-            game()
-        else:
-            print("Sorry, that's wrong. Final score: {score}")
-            return
+            if A['follower_count'] < B['follower_count']:
+                score += 1
+                print(f"You're right! Current score: {score}")
+                game()
+            else:
+                print(f"Sorry, that's wrong. Final score: {score}")
+                return
+
+game()
+score_calculator()
