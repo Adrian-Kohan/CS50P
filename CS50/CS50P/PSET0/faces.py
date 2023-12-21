623b11ca2293a40527d8b4a77b9b65682f05c4a4
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-21 09:12:00 +0000

    Thu, Dec 21, 2023, 12:42 PM +03:30

--- CS50/CS50P/PSET0/faces.py
+++ CS50/CS50P/PSET0/faces.py
@@ -1,5 +1,7 @@
 def convert(text):
-    if ":)" in text:
+    if ":)" in text and ":(" in text:
+        return text.replace(":)" ,"🙂") and text.replace(":(" ,"🙁")
+    elif ":)" in text:
         return text.replace(":)" ,"🙂")
     elif ":(" in text:
         return text.replace(":(" ,"🙁")
