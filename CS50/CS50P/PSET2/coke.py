b1fe02fc3055dca7866e2cc631bee26393c504db
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-23 13:37:01 +0000

    Sat, Dec 23, 2023, 5:07 PM +03:30

--- CS50/CS50P/PSET2/coke.py
+++ CS50/CS50P/PSET2/coke.py
@@ -1,10 +1,12 @@
 coke = 50
 
 
-while coke != 0:
-    if coin == 5 or coin == 10 or coin == 25:
+if coin == 5 or coin == 10 or coin == 25:
+    while coke != 0:
         coke = coke - coin
         print(f"Amount Due: {coke}")
         coin = input(int("Inser Coin: "))
+    print("Change Owed: ")
 
-print("Change Owed: ")
+else:
+    pass
