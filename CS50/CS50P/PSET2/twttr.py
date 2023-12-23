2ee43e581f06694954bb811111fdf6fbbf8f6fa0
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-23 13:57:05 +0000

    Sat, Dec 23, 2023, 5:27 PM +03:30

---
+++ CS50/CS50P/PSET2/twttr.py
@@ -0,0 +1,8 @@
+input = input("Input: ")
+vowels = ["o", "a", "u", "e", "i", "O", "A", "U", "E", "I", "O"]
+
+for char in input:
+    if char in vowels:
+        input = input.replace(char, "")
+
+print(f"Output: {input}")
