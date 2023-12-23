b53847445a89bbbe3612a61118e4bcfbd230899a
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-23 08:21:48 +0000

    Sat, Dec 23, 2023, 11:51 AM +03:30

--- CS50/CS50P/PSET2/camel.py
+++ CS50/CS50P/PSET2/camel.py
@@ -1,9 +1,9 @@
 camelCase = input("camelCase: ")
 
-for ch in camelCase:
-    while ch.isupper():
-        snake_case = camelCase.replace(ch, f"_{ch.lower()}")
-    print(f"snake_case: {snake_case}")
-    if not ch.isupper():
-        snake_case = camelCase
-        print(f"snake_case: {snake_case}")
+while camelCase.isupper():
+    for char in camelCase:
+        camelCase = camelCase.replace(ch, f"_{ch.lower()}")
+
+print(f"snake_case: {camelCase}")
+
+    
