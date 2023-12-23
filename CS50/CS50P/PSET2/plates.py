c6bd571042a2d73dd169c04b6d20839acfacc29f
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-23 14:56:28 +0000

    Sat, Dec 23, 2023, 6:26 PM +03:30

--- CS50/CS50P/PSET2/plates.py
+++ CS50/CS50P/PSET2/plates.py
@@ -10,9 +10,15 @@ def is_valid(s):
     symbols = "'!?@#$%^&*()_-+=}{[]"'"'
     if 2 <= len(s) <= 6 and s[0].isalpha() and s[1].isalpha():
         for i in s:
-            if i not in symbols:
-                print(symbols)
-                return True
+            if i in symbols:
+                return False
+            if i.isnumeric() and s[-1].isnumeric():
+                    index_i = s.index(i)
+                    before_i = index_i - 1
+                    if s[before_i].isalpha() and s[index_i] !=0:
+                        return True
+                    elif i.isalpha():
+                        return True
 
 
 
