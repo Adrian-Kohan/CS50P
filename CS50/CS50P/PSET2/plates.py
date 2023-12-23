138cebc6cc65c903bfa658d3baa60cf0371bba53
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-23 14:14:06 +0000

    Sat, Dec 23, 2023, 5:44 PM +03:30

--- CS50/CS50P/PSET2/plates.py
+++ CS50/CS50P/PSET2/plates.py
@@ -10,8 +10,7 @@ def is_valid(s):
     index = 0
     if 2 <= len(s) <= 6 and s[0].isalpha() and s[1].isalpha and s[-1].isnumeric:
         for i in s:
-            index += 0
-            if i.isnumeric:
+            if i.isnumeric and
         return True
 
 
