67a26bf0127c97e97bbc91cbeb702f08e22bdad5
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-23 06:06:02 +0000

    Sat, Dec 23, 2023, 9:36 AM +03:30

--- CS50/CS50P/PSET1/deep.py
+++ CS50/CS50P/PSET1/deep.py
@@ -1,5 +1,5 @@
 answer = input("What is  the answer to the Great Question of Life, the Universe and Everything? ").lower()
-if answer == "42" or answer == "forty two" or answer == "forty-two":
+if "42" in answer or answer == "forty two" or answer == "forty-two":
     print("Yes")
 else:
     print("No")
