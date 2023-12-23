469bd54825edfa88c8659bdf13378332c477b674
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-23 06:48:13 +0000

    Sat, Dec 23, 2023, 10:18 AM +03:30

--- CS50/CS50P/PSET1/extensions.py
+++ CS50/CS50P/PSET1/extensions.py
@@ -1,3 +1,9 @@
 suffixes = ["gif", "jpg", "jpeg", "png", "pdf", "txt", "zip"]
 
 file_name = input("Flie name: ")
+if ".gif" in file_name:
+    print("image/gif")
+elif ".jpg" in file_name or ".jpeg" in file_name:
+    print("image/jpeg")
+elif ".png" in file_name:
+    print("image/png")
