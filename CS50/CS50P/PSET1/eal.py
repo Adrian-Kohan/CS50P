91b8e87e9c4848e623b9d997329bcbb9ad9ba0b7
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-23 07:29:20 +0000

    Sat, Dec 23, 2023, 10:59 AM +03:30

--- CS50/CS50P/PSET1/eal.py
+++ CS50/CS50P/PSET1/eal.py
@@ -1,9 +1,25 @@
 def main():
     time = input("What time is it? ")
+    time = conver(time)
+
+    if 7 <= time <= 8:
+        print("breakfast time")
+    elif 12 <= time <= 13:
+        print("lunch time")
+    elif 18 <= time <= 19:
+        print("dinner time")
+    else:
+        pass
 
 
 def convert(time):
     time_list = time.split(":")
+    hour = int(time_list[0])
+    min = int(time_list[1])
+    if min == 30:
+        return float(f"{hour}.5")
+    else:
+        return float(f"{hour}.{min}")
 
 
 if __name__ == "__main__":
