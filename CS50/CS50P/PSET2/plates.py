1b5b82bd1689cd5ef5b963e96217d6252913cd21
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-24 04:57:23 +0000

    Sun, Dec 24, 2023, 8:27 AM +03:30

--- CS50/CS50P/PSET2/plates.py
+++ CS50/CS50P/PSET2/plates.py
@@ -8,10 +8,15 @@ def main():
 
 def is_valid(s):
     symbols = "'!?@#$%^&*()/\_-+=}{[]"'"'
-    is_true = 0
+    is_true = False
+
     if 2 <= len(s) <= 6:
-        
-        if s[0].isalpha() and s[1].isalpha():
+        is_true = True
+    else:
+        is_true = False
+        break
+
+    if s[0].isalpha() and s[1].isalpha():
             for i in s:
                 if i not in symbols:
                     if i.isalpha():
