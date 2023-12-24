383d70942e2900b1f27742393df90061be904884
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2023-12-24 05:14:24 +0000

    Sun, Dec 24, 2023, 8:44 AM +03:30

--- CS50/CS50P/PSET2/plates.py
+++ CS50/CS50P/PSET2/plates.py
@@ -20,9 +20,10 @@ def is_valid(s):
                         elif i.isnumeric() and s[-1].isnumeric():
                             index_i = s.index(i)
                             before_i = index_i - 1
+                            after_i = index_i + 1
                             if s[before_i].isalpha() and s[index_i] != "0":
                                 is_true = True
-                            elif s[before_i].isnumeric() and s[index_i] == "0":
+                            elif s[before_i].isnumeric() and s[index_i] == "0" and s[after_i].isnumeric():
                                 is_true = True
                             else:
                                 is_true = False
