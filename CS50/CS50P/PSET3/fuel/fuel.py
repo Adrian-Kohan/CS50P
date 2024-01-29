5b816961703a94df295185afe651074bfdea9fa0
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-01-29 07:29:39 +0000

    Mon, Jan 29, 2024, 10:59 AM +03:30

--- CS50/CS50P/PSET3/fuel/fuel.py
+++ CS50/CS50P/PSET3/fuel/fuel.py
@@ -1,8 +1,8 @@
+fraction = input("Fraction: ")
+x = int(fraction[0])
+y = int(fraction[2])
 
 try:
-    fraction = input("Fraction: ")
-    x = int(fraction[0])
-    y = int(fraction[2])
     fuel = (x / y) * 100
 except (ValueError, ZeroDivisionError):
     fraction = input("Fraction: ")
