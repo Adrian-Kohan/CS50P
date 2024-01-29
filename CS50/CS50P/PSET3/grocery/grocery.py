34966628a0db3972c6d715eacb85f0191c057239
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-01-29 09:16:32 +0000

    Mon, Jan 29, 2024, 12:46 PM +03:30

--- CS50/CS50P/PSET3/grocery/grocery.py
+++ CS50/CS50P/PSET3/grocery/grocery.py
@@ -6,5 +6,5 @@ while True:
     except EOFError:
         shopping_list.sort()
         for item in shopping_list:
-            print(f"{count(item)} {item}")
+            print(f"{shopping_list.count(item)} {item}")
         break
