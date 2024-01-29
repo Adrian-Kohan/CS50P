5c5ccfe753745300f70cbccfa77c5a876264c014
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-01-29 10:56:16 +0000

    Mon, Jan 29, 2024, 2:26 PM +03:30

--- CS50/CS50P/PSET3/outdated/outdated.py
+++ CS50/CS50P/PSET3/outdated/outdated.py
@@ -21,8 +21,11 @@ while True:
             month = new_date[0]
             day = new_date[1]
             year = new_date[2]
-            if day <= 31:
+            if 10 <= day <= 31 and 10 <= month <= 12:
                 print(f"{year}-{month}-{day}")
+                break
+            elif 0 < day < 10 and 0 < month < 10:
+                print(f"{year}-0{month}-0{day}")
                 break
             else:
                 continue
@@ -32,8 +35,11 @@ while True:
             day = new_date[1].split(",")
             new_day = day[0]
             year = new_date[2]
-            if month.title() in months and int(new_day) <= 31:
+            if month.title() in months and 10 <= int(new_day) <= 31:
                 print(f"{year}-{months.index(month.title()) + 1}-{new_day}")
+                break
+            elif month.title() in months and 10 > int(new_day) > 0:
+                print(f"{year}-{months.index(month.title()) + 1}-0{new_day}")
                 break
             else:
                 continue
