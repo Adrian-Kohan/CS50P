c7d6053e0c9d0a2e1f625de492c841d39c53b916
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-01-29 07:50:33 +0000

    Mon, Jan 29, 2024, 11:20 AM +03:30

--- CS50/CS50P/PSET3/fuel/fuel.py
+++ CS50/CS50P/PSET3/fuel/fuel.py
@@ -4,21 +4,20 @@ def fun():
     y = int(fraction[2])
     return x, y
 
+fun()
 if x <= y:
-try:
-    fuel = (x / y) * 100
-except (ValueError, ZeroDivisionError):
-    fun()
-    fuel = (x / y) * 100
-finally:
-    if fuel <= 1:
-        print("E")
-    elif fuel >= 99:
-        print("F")
-    else:
-        print(f'{round(fuel)}%')
+    try:
+        fuel = (x / y) * 100
+    except (ValueError, ZeroDivisionError):
+        fun()
+        fuel = (x / y) * 100
+    finally:
+        if fuel <= 1:
+            print("E")
+        elif fuel >= 99:
+            print("F")
+        else:
+            print(f'{round(fuel)}%')
 
 else:
-    fraction = input("Fraction: ")
-    x = int(fraction[0])
-    y = int(fraction[2])
+    fun()
