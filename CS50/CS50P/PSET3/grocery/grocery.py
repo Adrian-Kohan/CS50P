a737e65c05f3ce197ab4c41ca31d4ce227ccede3
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-01-29 09:10:39 +0000

    Mon, Jan 29, 2024, 12:40 PM +03:30

--- CS50/CS50P/PSET3/grocery/grocery.py
+++ CS50/CS50P/PSET3/grocery/grocery.py
@@ -1,8 +1,10 @@
 shopping_list = []
 while True:
+    item = input("").lower()
     try:
-        item = input().lower()
         shopping_list.append(item.upper())
     except EOFError:
         shopping_list.sort()
+        for item in shopping_list:
+            print(item)
         break
