e41e1684a5ff9dad8645822b5eec7c661022391d
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-01-29 10:43:43 +0000

    Mon, Jan 29, 2024, 2:13 PM +03:30

--- CS50/CS50P/PSET3/outdated/outdated.py
+++ CS50/CS50P/PSET3/outdated/outdated.py
@@ -21,12 +21,18 @@ while True:
             month = new_date[0]
             day = new_date[1]
             year = new_date[2]
-
+            print(f"{year}/{month}/{day}")
+            break
         elif "," in date:
             new_date = date.split(" ")
             month = new_date[0]
-            day = new_date[1]
+            day = new_date[1].split(",")
+            new_day = day[0]
             year = new_date[2]
-
+            if month.title() in months:
+                print(f"{year}/{months.index(month) + 1}/{new_day}")
+                break
+            else:
+                continue
     except :
         continue
