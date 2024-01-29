4ccc8f7e118d47eec095328713a7f1f22b13029b
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-01-29 11:00:04 +0000

    Mon, Jan 29, 2024, 2:30 PM +03:30

--- CS50/CS50P/PSET3/outdated/outdated.py
+++ CS50/CS50P/PSET3/outdated/outdated.py
@@ -18,9 +18,9 @@ while True:
     try:
         if "/" in date:
             new_date = date.split("/")
-            month = new_date[0]
-            day = new_date[1]
-            year = new_date[2]
+            month = int(new_date[0])
+            day = int(new_date[1])
+            year = int(new_date[2])
             if 10 <= day <= 31 and 10 <= month <= 12:
                 print(f"{year}-{month}-{day}")
                 break
@@ -33,13 +33,13 @@ while True:
             new_date = date.split(" ")
             month = new_date[0]
             day = new_date[1].split(",")
-            new_day = day[0]
+            new_day = int(day[0])
             year = new_date[2]
-            if month.title() in months and 10 <= int(new_day) <= 31:
+            if month.title() in months and 10 <= new_day <= 31 and 10 <= months.index(month.title()) <= 12:
                 print(f"{year}-{months.index(month.title()) + 1}-{new_day}")
                 break
-            elif month.title() in months and 10 > int(new_day) > 0:
-                print(f"{year}-{months.index(month.title()) + 1}-0{new_day}")
+            elif month.title() in months and 10 > new_day > 0 and 0 < months.index(month.title()) < 10:
+                print(f"{year}-0{months.index(month.title()) + 1}-0{new_day}")
                 break
             else:
                 continue
