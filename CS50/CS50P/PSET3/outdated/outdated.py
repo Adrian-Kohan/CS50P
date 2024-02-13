4c763c252e937488d8784dd4052a4da6447a13c1
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-13 05:36:47 +0000

    Tue, Feb 13, 2024, 9:06 AM +03:30

--- CS50/CS50P/PSET3/outdated/outdated.py
+++ CS50/CS50P/PSET3/outdated/outdated.py
@@ -22,31 +22,32 @@ while True:
             day = new_date[1]
             year = new_date[2]
 
-            if int(day) <= 31:
+            if int(day) <= 31 and int(month) < 13:
                 if 0 < int(day) < 10:
-                    day = 0 + day
+                    day = "0" + day
 
                 if 0 < int(month) < 10:
-                    month = 0 + month
+                    month = "0" + month
 
                 print(f"{year}-{month}-{day}")
                 break
 
         elif "," in date:
             new_date = date.split(" ")
+            year = new_date[2]
             month = new_date[0].title()
+            new_month = months.index(month) + 1
             day = new_date[1].split(",")
             new_day = day[0]
-            year = new_date[2]
 
             if month in months and int(new_day) <= 31:
                 if 0 < int(new_day) < 10:
-                    new_day = 0 + new_day
+                    new_day = "0" + new_day
 
-                if 1 <= months.index(month) < 10:
-                    month = 0 + str(months.index(month) + 1)
+                if 0 < new_month < 10:
+                    new_month = "0" + str(new_month)
 
-                print(f"{year}-{month}-{new_day}")
+                print(f"{year}-{new_month}-{new_day}")
                 break
     except:
         continue
