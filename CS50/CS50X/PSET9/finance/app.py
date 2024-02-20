65776bd4369ad46bf434439b6b86e140212cf512
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-20 12:55:16 +0000

    Tue, Feb 20, 2024, 4:25 PM +03:30

--- CS50/CS50X/PSET9/finance/app.py
+++ CS50/CS50X/PSET9/finance/app.py
@@ -365,13 +365,14 @@ def add():
 
         cash = db.execute("SELECT cash FROM users WHERE id = ?", session.get("user_id"))
         cash = cash[0]["cash"]
-        additional_cash = int(request.form.get("cash"))
-
-        # Ensure additional cash is not empty submitted
-        if not additional_cash:
-            return apology("must enter some cash", 403)
 
         try:
+            additional_cash = int(request.form.get("cash"))
+
+            # Ensure additional cash is not empty submitted
+            if not additional_cash:
+                return apology("must enter some cash", 403)
+            
              # Ensure amount of cash is a positive number
             if additional_cash < 0:
                 return apology("amount of cash must be positive", 403)
