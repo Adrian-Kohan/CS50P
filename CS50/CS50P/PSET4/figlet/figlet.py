347472464ea04900c9263e9ebf194619ae1dd76c
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-22 12:34:23 +0000

    Thu, Feb 22, 2024, 4:04 PM +03:30

--- CS50/CS50P/PSET4/figlet/figlet.py
+++ CS50/CS50P/PSET4/figlet/figlet.py
@@ -5,5 +5,11 @@ from pyfiglet import Figlet
 text = input("Input")
 
 try:
-    f = Figlet(font=)
-    print f.renderText('text to render')
+    if len(sys.argv) == 2:
+        f = Figlet(font=sys.argv[1])
+        print f.renderText(f"Output: {text}")
+    elif len(sys.argv) == 0:
+        print renderText(f"Output: {text}")
+except:
+    print("Error")
+    sys.exit
