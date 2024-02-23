9a5d02cfc0b5d8eab8dc211206739301c720c3b7
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-23 07:29:12 +0000

    Fri, Feb 23, 2024, 10:59 AM +03:30

--- CS50/CS50P/PSET4/figlet/figlet.py
+++ CS50/CS50P/PSET4/figlet/figlet.py
@@ -1,18 +1,20 @@
 import sys
-import pyfiglet
+from pyfiglet import Figlet
 import random
 
+figlet = Figlet()
 
 text = input("Input: ")
-print(pyfiglet.figlet_format(f"Output: {text}"))
 
 try:
     if len(sys.argv) == 2:
-        f = Figlet(font=sys.argv[1])
-        print(pyfiglet.figlet_format(f"Output: {text}", font=f))
+        font = Figlet.setFont(font=sys.argv[1])
+        print(f"Output: {figlet.renderText(text, font=font)}")
     elif len(sys.argv) == 0:
-        f = pyfiglet.figlet.setFont(font=random.choice(pyfiglet.figlet.getFonts()))
-        print(pyfiglet.figlet_format(f"Output: {text}", font=f))
+        fonts = figlet.getFonts()
+        font = figlet.setFont(font=random.choice(fonts))
+        print(f"Output: {figlet.renderText(text, font=font)}")
+
 except:
     print("Error")
     sys.exit
