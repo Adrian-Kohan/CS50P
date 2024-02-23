9d4d2b57c2f77f5d827ad77a9d02b6600653b5e5
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-23 07:32:03 +0000

    Fri, Feb 23, 2024, 11:02 AM +03:30

--- CS50/CS50P/PSET4/figlet/figlet.py
+++ CS50/CS50P/PSET4/figlet/figlet.py
@@ -6,15 +6,15 @@ figlet = Figlet()
 
 text = input("Input: ")
 
-try:
-    if len(sys.argv) == 2:
-        font = Figlet.setFont(font=sys.argv[1])
-        print(f"Output: {figlet.renderText(text, font=font)}")
-    elif len(sys.argv) == 0:
-        fonts = figlet.getFonts()
-        font = figlet.setFont(font=random.choice(fonts))
-        print(f"Output: {figlet.renderText(text, font=font)}")
 
+if len(sys.argv) == 2:
+    font = Figlet.setFont(font=sys.argv[1])
+elif len(sys.argv) == 0:
+    fonts = figlet.getFonts()
+    font = figlet.setFont(font=random.choice(fonts))
+
+try:
+    print(f"Output: {figlet.renderText(text)}")
 except:
     print("Error")
     sys.exit
