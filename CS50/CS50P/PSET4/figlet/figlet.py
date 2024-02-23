9d4e4d74263525ae1d98b4ef45da2cca3a2fa2f3
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-23 07:46:44 +0000

    Fri, Feb 23, 2024, 11:16 AM +03:30

--- CS50/CS50P/PSET4/figlet/figlet.py
+++ CS50/CS50P/PSET4/figlet/figlet.py
@@ -1,6 +1,6 @@
 import sys
 from pyfiglet import Figlet
-from random import 
+import random
 
 figlet = Figlet()
 fonts = figlet.getFonts()
@@ -8,7 +8,7 @@ fonts = figlet.getFonts()
 
 
 
-if len(sys.argv) == 2:
+if len(sys.argv) == 3:
     if sys.argv[0] == "-f" or sys.argv[0] == "--font":
         if sys.argv[1] in fonts:
             text = input("Input: ")
@@ -21,7 +21,7 @@ if len(sys.argv) == 2:
         print("Invalid usage")
         sys.exit
 
-elif len(sys.argv) == 0:
+elif len(sys.argv) == 1:
     text = input("Input: ")
     font = figlet.setFont(font=random.choice(fonts))
     print(f"Output:\n{figlet.renderText(text)}")
