71ce53d8719d074aabdc935cdc9fb9fbe7f930f7
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-25 09:55:58 +0000

    Sun, Feb 25, 2024, 1:25 PM +03:30

--- CS50/CS50P/PSET5/test_twttr/twttr.py
+++ CS50/CS50P/PSET5/test_twttr/twttr.py
@@ -7,15 +7,11 @@ def main():
 def shorten(word):
     vowels = ["o", "a", "u", "e", "i", "O", "A", "U", "E", "I"]
     new_word = word
-    for char in word:
+    for char in new_word:
         if char in vowels:
-            new_word = new_word.remove(char, "")
+            new_word = new_word.replace(char, "")
             return new_word
 
 
 if __name__ == "__main__":
     main()
-
-
-
-print(f"Output: {input}")
