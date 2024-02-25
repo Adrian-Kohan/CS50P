709a5cb566990e1f85c70d17dc0761448e174feb
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-25 11:25:13 +0000

    Sun, Feb 25, 2024, 2:55 PM +03:30

--- CS50/CS50P/PSET5/test_plates/test_plates.py
+++ CS50/CS50P/PSET5/test_plates/test_plates.py
@@ -1,5 +1,25 @@
 from plates import is_valid
 
-def test_start_with_letter:
+def test_start_with_letter():
     assert is_valid("cs50") == True
+    assert is_valid("222aA") == False
+
+def test_lenght():
     assert is_valid("cs50") == True
+    assert is_valid("cssffladf50") == False
+    assert is_valid("H") == False
+
+
+def test_number():
+    assert is_valid("cs50") == True
+    assert is_valid("cs50p") == False
+    assert is_valid("cs050") == False
+
+
+def test_punctuation_marks():
+    assert is_valid("cs50") == True
+    assert is_valid("cs/.50") == False
+
+def test_spaces():
+    assert is_valid("cs50") == True
+    assert is_valid("cs 50") == False
