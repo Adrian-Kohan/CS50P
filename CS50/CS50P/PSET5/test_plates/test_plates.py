9917bb4981f547ecfc9e1b77885be161c31dab7f
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-25 11:36:33 +0000

    Sun, Feb 25, 2024, 3:06 PM +03:30

--- CS50/CS50P/PSET5/test_plates/test_plates.py
+++ CS50/CS50P/PSET5/test_plates/test_plates.py
@@ -2,7 +2,10 @@ from plates import is_valid
 
 def test_start_with_letter():
     assert is_valid("cs50") == True
+    assert is_valid("cs") == True
     assert is_valid("222aA") == False
+    assert is_valid("222") == False
+
 
 def test_lenght():
     assert is_valid("cs50") == True
