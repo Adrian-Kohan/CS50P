67f510aa1cc940ba3080f2496944d579c523a664
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-25 10:36:28 +0000

    Sun, Feb 25, 2024, 2:06 PM +03:30

--- CS50/CS50P/PSET5/test_bank/test_bank.py
+++ CS50/CS50P/PSET5/test_bank/test_bank.py
@@ -1,1 +1,20 @@
-def test_
+from bank import value
+
+def test_with_hello():
+    assert value("hello, there") == "$0"
+    assert value("Hello, there") == "$0"
+    assert value("HELLO, there") == "$0"
+
+def test_with_h_at_first():
+    assert value("hi, there") == "$20"
+    assert value("Hi, there") == "$20"
+    assert value("HI, there") == "$20"
+
+def test_without_h():
+    assert value("WHAT'S UP") == "$100"
+    assert value("what's up") == "$100"
+    assert value("123") == "$100"
+
+
+
+
