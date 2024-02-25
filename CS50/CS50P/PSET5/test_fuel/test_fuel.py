40291b3b60ab400ba86aee2947eac15f1601896b
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-25 12:39:07 +0000

    Sun, Feb 25, 2024, 4:09 PM +03:30

--- CS50/CS50P/PSET5/test_fuel/test_fuel.py
+++ CS50/CS50P/PSET5/test_fuel/test_fuel.py
@@ -1,9 +1,9 @@
 from fuel import convert, gauge
 
 def test_correct_input():
-    assert convert("1/2") == "50%"
-    assert convert("1/1") == "F"
-    assert convert("1/100") == "E"
+    assert convert("1/2") == 50
+    assert convert("1/1") == 100
+    assert convert("1/100") == 0.01
 
 def test_incorrect_input():
     assert convert("100/1") == ValueError
