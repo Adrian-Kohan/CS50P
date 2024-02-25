076a4b713d795956bd8c44c66aaff97b5afd7855
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-25 11:39:36 +0000

    Sun, Feb 25, 2024, 3:09 PM +03:30

---
+++ CS50/CS50P/PSET5/test_fuel/fuel.py
@@ -0,0 +1,34 @@
+def main():
+    ...
+
+
+def convert(fraction):
+    ...
+
+
+def gauge(percentage):
+    ...
+
+
+if __name__ == "__main__":
+    main()
+
+while True:
+    fraction = input("Fraction: ").split("/")
+    try:
+        x = int(fraction[0])
+        y = int(fraction[1])
+        if x <= y:
+            fuel = (x / y) * 100
+            if fuel <= 1:
+                print("E")
+                break
+            elif fuel >= 99:
+                print("F")
+                break
+            else:
+                print(f'{round(fuel)}%')
+                break
+
+    except (ValueError, ZeroDivisionError):
+            continue
