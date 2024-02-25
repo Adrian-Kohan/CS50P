88e087de376e75e2887b08bd3345b9e84f4d9978
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-25 09:43:38 +0000

    Sun, Feb 25, 2024, 1:13 PM +03:30

--- CS50/CS50P/PSET5/test_twttr/test_twttr.py
+++ CS50/CS50P/PSET5/test_twttr/test_twttr.py
@@ -1,2 +1,11 @@
 form twttr import shorten
 
+def cap():
+    assert shorten("HELLO") == "HLL"
+
+def un_cap():
+    assert shorten("hello") == "hll"
+
+def without():
+    assert shorten("123") == "123"
+    
