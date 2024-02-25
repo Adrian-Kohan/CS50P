b89e112e94b06359bd4417737db7cf72b1b16141
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-25 10:19:38 +0000

    Sun, Feb 25, 2024, 1:49 PM +03:30

--- CS50/CS50P/PSET5/test_twttr/test_twttr.py
+++ CS50/CS50P/PSET5/test_twttr/test_twttr.py
@@ -1,10 +1,10 @@
 from twttr import shorten
 
-def cap_vowel():
+def test_cap_vowel():
     assert shorten("HELLO") == "HLL"
 
-def un_cap_vowel():
+def test_un_cap_vowel():
     assert shorten("hello") == "hll"
 
-def without_vowel():
+def test_without_vowel():
     assert shorten("123") == "123"
