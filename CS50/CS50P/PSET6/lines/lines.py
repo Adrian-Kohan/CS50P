e5da8687512bf0ca0e30900541a2dd106eece237
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-26 14:18:03 +0000

    Mon, Feb 26, 2024, 5:48 PM +03:30

--- CS50/CS50P/PSET6/lines/lines.py
+++ CS50/CS50P/PSET6/lines/lines.py
@@ -2,22 +2,28 @@ import sys
 
 
 try:
-if len(sys.argv) == 1:
-    sys.exit("Too few command-line arguments")
+    if len(sys.argv) == 1:
+        sys.exit("Too few command-line arguments")
 
-elif ".py" not in sys.argv[1]:
-    sys.exit("Not a python file")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
 
-else:
-    with open(sys.argv[1], "r") as code:
-        data = code.readlines()
-        lines = 0
-        for i in data:
-            if i[0] == "#":
-                pass
-            elif i == "\n":
-                pass
-            else:
-                lines += 1
+    elif ".py" not in sys.argv[1]:
+        sys.exit("Not a python file")
 
-        print(lines)
+    else:
+        with open(sys.argv[1], "r") as code:
+            data = code.readlines()
+            lines = 0
+            for i in data:
+                if i[0] == "#":
+                    pass
+                elif i == "\n":
+                    pass
+                else:
+                    lines += 1
+
+            print(lines)
+
+except(FileNotFoundError):
+    print("File does not exist")
