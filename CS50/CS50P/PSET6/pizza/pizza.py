81a564a87bc74d27d1ab7fdb86d06c2e05c6a11d
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-26 15:20:06 +0000

    Mon, Feb 26, 2024, 6:50 PM +03:30

--- CS50/CS50P/PSET6/pizza/pizza.py
+++ CS50/CS50P/PSET6/pizza/pizza.py
@@ -1,2 +1,22 @@
 import sys
 from tabulate import tabulate
+
+
+if len(sys.argv) == 1:
+    sys.exit("Too few command-line arguments")
+
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+
+elif ".py" not in sys.argv[1]:
+    sys.exit("Not a python file")
+
+else:
+    try:
+        with open(sys.argv[1], "r") as table:
+            table = code.read()
+            print(tabulate(table, headers, tablefmt="grid"))
+
+
+    except(FileNotFoundError):
+        print("File does not exist")
