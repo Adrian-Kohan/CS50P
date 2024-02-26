4906932ec8d227388421b635aea69afb1a6f0f50
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-26 13:54:08 +0000

    Mon, Feb 26, 2024, 5:24 PM +03:30

--- CS50/CS50P/PSET6/lines/lines.py
+++ CS50/CS50P/PSET6/lines/lines.py
@@ -1,3 +1,11 @@
 import sys
 
-with opnen
+
+if len(sys.argv) == 0:
+    print("Too few command-line arguments")
+
+if ".py" is not in sys.argv[1]:
+    print("Not a python file")
+
+with open(sys.argv[1], "r") as code:
+    data = json.load(code)
