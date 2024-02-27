540b6726ec8a7fcc947230fc749c5967e1b195d6
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-27 08:04:57 +0000

    Tue, Feb 27, 2024, 11:34 AM +03:30

--- CS50/CS50P/PSET6/scourgify/scourgify.py
+++ CS50/CS50P/PSET6/scourgify/scourgify.py
@@ -14,19 +14,18 @@ else:
         with open(sys.argv[1], "r") as data_1:
             dataset1 = list(csv.DictReader(data_1, delimiter=","))
 
-            dataset2 = []
-
             for i in dataset1:
                 new_name = i["name"].split(", ")
                 first = new_name[1]
                 last = new_name[0]
                 house = i["house"]
-                dataset2.append({"first": first, "last": last, "house": house})
 
-        with open('after.csv', 'w', newline='') as data_2:
-            writer = csv.DictWriter(data_2, fieldnames=dataset2.keys())
-            writer.writeheader()
-            writer.writerow(dataset2)
+                new_data = {"first": first, "last": last, "house": house}
+
+                with open('after.csv', 'w', newline='') as data_2:
+                    writer = csv.DictWriter(data_2, fieldnames=new_data.keys())
+                    writer.writeheader()
+                    writer.writerow(new_data)
 
     except(FileNotFoundError):
         sys.exit(f"Could not read {sys.argv[1]}")
