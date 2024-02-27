2dc3e57469dc15732f16336f6fd9ef9e5719b089
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-27 08:38:05 +0000

    Tue, Feb 27, 2024, 12:08 PM +03:30

--- CS50/CS50P/PSET6/shirt/shirt.py
+++ CS50/CS50P/PSET6/shirt/shirt.py
@@ -11,23 +11,11 @@ elif len(sys.argv) > 3:
 
 else:
     try:
-        dataset2 = []
-        with open(sys.argv[1], "r") as data_1:
-            dataset1 = list(csv.DictReader(data_1, delimiter=","))
+        with Image.open(sys.argv[1]) as im:
+            im.thumbnail(size)
+            im.save(file + ".thumbnail", "JPEG")
 
-            for i in dataset1:
-                new_name = i["name"].split(",")
-                first = new_name[1]
-                last = new_name[0]
-                house = i["house"]
 
-                dataset2.append({"first": first.lstrip(), "last": last.lstrip(), "house": house})
-
-        with open(sys.argv[2], 'w') as data_2:
-                writer = csv.DictWriter(data_2, fieldnames=dataset2[0].keys())
-                writer.writeheader()
-                for i in dataset2:
-                    writer.writerow(i)
 
     except(FileNotFoundError):
         sys.exit(f"Could not read {sys.argv[1]}")
