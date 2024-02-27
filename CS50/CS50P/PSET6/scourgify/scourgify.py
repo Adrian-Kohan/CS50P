5ecc28383c187c56b230efac225f6521c8de5670
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-27 07:44:00 +0000

    Tue, Feb 27, 2024, 11:14 AM +03:30

--- CS50/CS50P/PSET6/scourgify/scourgify.py
+++ CS50/CS50P/PSET6/scourgify/scourgify.py
@@ -11,10 +11,10 @@ elif len(sys.argv) > 3:
 
 else:
     try:
-        print(sys.argv[1])
-        print(sys.argv[2])
         with open(sys.argv[1], "r") as data_1:
             dataset1 = list(csv.DictReader(data_1, delimiter=","))
+            for i in dataset1:
+                new_name = i["name"].split(",")
 
         with open(sys.argv[2], "r") as data_2:
             dataset2 = list(csv.DictReader(data_2, delimiter=","))
@@ -24,4 +24,4 @@ else:
 
 
     except(FileNotFoundError):
-        sys.exit("File does not exist")
+        sys.exit(f"Could not read {sys.argv[1]}")
