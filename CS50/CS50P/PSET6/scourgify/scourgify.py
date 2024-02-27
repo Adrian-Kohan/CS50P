0dd765eee3f5ec6db501ec5a7632347fcb3578fe
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-27 07:57:44 +0000

    Tue, Feb 27, 2024, 11:27 AM +03:30

--- CS50/CS50P/PSET6/scourgify/scourgify.py
+++ CS50/CS50P/PSET6/scourgify/scourgify.py
@@ -13,14 +13,15 @@ else:
     try:
         with open(sys.argv[1], "r") as data_1:
             dataset1 = list(csv.DictReader(data_1, delimiter=","))
+        with open(sys.argv[2], "w") as data_2:
+            dataset2 = list(csv.DictReader(data_2, delimiter=","))
+
             for i in dataset1:
                 new_name = i["name"].split(", ")
-                with open(sys.argv[2], "w") as data_2:
-                    dataset2 = list(csv.DictReader(data_2, delimiter=","))
-                    first = new_name[0]
-                    last = new_name[1]
-                    house = i["house"]
-                    dataset2.append({"first": first, "last": last, "house": house})
+                first = new_name[0]
+                last = new_name[1]
+                house = i["house"]
+                dataset2.append({"first": first, "last": last, "house": house})
 
 
 
