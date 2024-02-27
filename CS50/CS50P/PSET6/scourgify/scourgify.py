cea7035d6a1b022f27f5802b06a254f8ef89e84a
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-27 07:36:44 +0000

    Tue, Feb 27, 2024, 11:06 AM +03:30

--- CS50/CS50P/PSET6/scourgify/scourgify.py
+++ CS50/CS50P/PSET6/scourgify/scourgify.py
@@ -11,12 +11,12 @@ elif len(sys.argv) > 3:
 
 else:
     try:
-        with open(sys.argv[1], "r") as data_1:
+        with open(sys.argv[1], sys.argv[2], "r") as data_1:
             dataset1 = list(csv.DictReader(data_1, delimiter=","))
-
-        with open(sys.argv[2], "r") as data_2:
             dataset2 = list(csv.DictReader(data_2, delimiter=","))
 
+            print(dataset1)
+            print(dataset2)
 
 
     except(FileNotFoundError):
