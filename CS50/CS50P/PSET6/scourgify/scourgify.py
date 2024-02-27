c000446f6c31ecd98798212503e0276224aeea6b
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-27 07:39:48 +0000

    Tue, Feb 27, 2024, 11:09 AM +03:30

--- CS50/CS50P/PSET6/scourgify/scourgify.py
+++ CS50/CS50P/PSET6/scourgify/scourgify.py
@@ -13,8 +13,10 @@ else:
     try:
         print(sys.argv[1])
         print(sys.argv[2])
-        with open(sys.argv[1], sys.argv[2], "r") as data_1:
+        with open(sys.argv[1], "r") as data_1:
             dataset1 = list(csv.DictReader(data_1, delimiter=","))
+
+        with open(sys.argv[2], "r") as data_2:
             dataset2 = list(csv.DictReader(data_2, delimiter=","))
 
             print(dataset1)
