798095d4668d7cc7989fba5a583a3e8c3fd6522d
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-28 08:32:13 +0000

    Wed, Feb 28, 2024, 12:02 PM +03:30

--- CS50/CS50P/PSET7/numb3rs/test_numb3rs.py
+++ CS50/CS50P/PSET7/numb3rs/test_numb3rs.py
@@ -2,8 +2,10 @@ from numb3rs import validate
 
 def test_correct_number():
     assert validate("1.1.1.1") == True
+    assert validate("255.255.255.255") == True
     assert validate("257.1.1.1") == False
     assert validate("1.1.1") == False
 
 
-def test_
+def test_alphabet():
+    assert validate("cs50") == False
