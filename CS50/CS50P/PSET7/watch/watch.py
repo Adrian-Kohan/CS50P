ed22187ea38eed7e81c4c289747a5d78f2aad4dc
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-28 16:06:27 +0000

    Wed, Feb 28, 2024, 7:36 PM +03:30

--- CS50/CS50P/PSET7/watch/watch.py
+++ CS50/CS50P/PSET7/watch/watch.py
@@ -7,7 +7,7 @@ def main():
 
 
 def parse(s):
-    if matches := re.search(r'^<iframe\w([a-z0-9_]="."\w)*src="https?://(?:www\.)?youtube.com/embed/([a-z0-9_]+)".*></iframe>$',s):
+    if matches := re.search(r'^<iframe( \w="\w")* src="https?://(?:www\.)?youtube.com/embed/(\w+)"( \w="\w")*></iframe>$',s):
         return f"https://youtu.be/{matches.group(1)}"
     else:
         sys.exit
