242fab2198d5dbdb5f24b18ad6fd5be33147e4e4
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-28 15:42:30 +0000

    Wed, Feb 28, 2024, 7:12 PM +03:30

--- CS50/CS50P/PSET7/watch/watch.py
+++ CS50/CS50P/PSET7/watch/watch.py
@@ -7,7 +7,7 @@ def main():
 
 
 def parse(s):
-    if matches := re.search(r'^src="https?://(?:www\.)?youtube.com/embed/xvFZjo5PgG0"$',s):
+    if matches := re.search(r'^src="https?://(?:www\.)?youtube.com/embed/([a-z0-9_]+)"$',s):
 
 
 
