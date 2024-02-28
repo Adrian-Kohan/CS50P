8e9041b8d3bacc938ff3a96c4d30f728e4436d66
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-28 05:20:01 +0000

    Wed, Feb 28, 2024, 8:50 AM +03:30

--- CS50/CS50P/PSET6/shirt/shirt.py
+++ CS50/CS50P/PSET6/shirt/shirt.py
@@ -21,7 +21,7 @@ else:
             print(size)
 
         with Image.open("shirt.png") as im2:
-            ImageOps.fit(im2, size, method=Resampling.BICUBIC, bleed=0.0, centering=(0.5, 0.5))
+            ImageOps.fit(im2, size, method=PIL.Image.Resampling.BICUBIC, bleed=0.0, centering=(0.5, 0.5))
             Image.paste(im1, box=None, mask=None)
             im2.save(sys.argv[2])
 
