0efa9313b936c85147ebf7f4f56f728a7a431abb
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-28 08:58:39 +0000

    Wed, Feb 28, 2024, 12:28 PM +03:30

--- CS50/CS50P/PSET7/numb3rs/test_numb3rs.py
+++ CS50/CS50P/PSET7/numb3rs/test_numb3rs.py
@@ -4,6 +4,9 @@ def test_correct_number():
     assert validate("1.1.1.1") == True
     assert validate("255.255.255.255") == True
     assert validate("257.1.1.1") == False
+    assert validate("2.1.1.257") == False
+    assert validate("4.257.1.1") == False
+    assert validate("4.1.257.1") == False
     assert validate("1.1.1") == False
 
 
