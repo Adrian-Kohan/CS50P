74865ef282f7fd5fa39860ac284c36441827cfb7
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-28 08:57:34 +0000

    Wed, Feb 28, 2024, 12:27 PM +03:30

--- CS50/CS50P/PSET7/numb3rs/numb3rs.py
+++ CS50/CS50P/PSET7/numb3rs/numb3rs.py
@@ -7,7 +7,7 @@ def main():
 
 def validate(ip):
     if matches := re.search(r"^([0-9][0-9]?[0-9]?)\.([0-9][0-9]?[0-9]?)\.([0-9][0-9]?[0-9]?)\.([0-9][0-9]?[0-9]?)$",ip):
-        if int(matches.group(1)) <= 255 and int(matches.group(1)) <= 255 and int(matches.group(1)) <= 255 and int(matches.group(1)) <= 255:
+        if int(matches.group(1)) <= 255 and int(matches.group(2)) <= 255 and int(matches.group(3)) <= 255 and int(matches.group(4)) <= 255:
             return True
         else:
             return False
