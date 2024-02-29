39de403a2788bf8b88ed03154cc4158cdebb7f57
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-29 07:22:20 +0000

    Thu, Feb 29, 2024, 10:52 AM +03:30

--- CS50/CS50P/PSET7/working/working.py
+++ CS50/CS50P/PSET7/working/working.py
@@ -8,7 +8,7 @@ def main():
 
 def convert(s):
     if matches := re.search(r'^([1-9]{1}[0-9]?(?::00)?) (A?P?M) to ([1-9]{1}[0-9]?(?::00)?) (A?P?M)$',s):
-        if "PM" in matches.group(1):
+        if "PM" in matches.group(2):
 
 
 
