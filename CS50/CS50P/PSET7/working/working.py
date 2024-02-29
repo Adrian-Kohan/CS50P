b1ab7771b673fc40635e064f6c76f743bbea3748
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-02-29 07:31:27 +0000

    Thu, Feb 29, 2024, 11:01 AM +03:30

--- CS50/CS50P/PSET7/working/working.py
+++ CS50/CS50P/PSET7/working/working.py
@@ -7,9 +7,15 @@ def main():
 
 
 def convert(s):
-    if matches := re.search(r'^([0-9]{1}[0-9]?(?::[0-9][0-9])?) (A?P?M) to ([0-9]{1}[0-9]?(?::[0-9][0-9])?) (A?P?M)$',s):
-        if "PM" in matches.group(2):
-            
+    time1 = ""
+    time2 = ""
+    if matches := re.search(r'^([0-9]{1}[0-9]?):?([0-9]?[0-9]?) (A?P?M) to ([0-9]{1}[0-9]?):?([0-9]?[0-9]?) (A?P?M)$',s):
+        if "PM" in matches.group(3):
+            time1 = f"{int(matches.group(1)) + 12}:{matches.group(2)}"
+
+        elif "PM" in matches.group(6):
+            time2 = f"{int(matches.group(1)) + 12}:{matches.group(2)}"
+
 
 
 ...
