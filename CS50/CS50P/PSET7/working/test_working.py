8ffb6f0a24e070f4c6585350349f83b9bbc5fc81
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-02 15:16:56 +0000

    Sat, Mar 2, 2024, 6:46 PM +03:30

--- CS50/CS50P/PSET7/working/test_working.py
+++ CS50/CS50P/PSET7/working/test_working.py
@@ -1,15 +1,15 @@
 from working import convert
 
-def test_correct_input_time:
+def test_correct_input_time():
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
 
-def test_incorrect_input_time:
+def test_incorrect_input_time():
     with pytest.raises(ValueError):
         assert convert("09:00 AM - 17:00 PM")
         assert convert("9 AM - 5 PM")
 
 
-def test_incorrect_time:
+def test_incorrect_time():
     with pytest.raises(ValueError):
         assert convert("9:60 AM to 5:60 PM")
