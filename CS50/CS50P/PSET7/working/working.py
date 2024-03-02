87e9eaaecd975a390ad900928b466c491de5a1ae
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-02 15:26:47 +0000

    Sat, Mar 2, 2024, 6:56 PM +03:30

--- CS50/CS50P/PSET7/working/working.py
+++ CS50/CS50P/PSET7/working/working.py
@@ -14,31 +14,28 @@ def convert(s):
 
         h2 = matches.group(4)
         m2 = matches.group(5)
-        if
-        if int(h1) < 24 and int(h2):
 
-            if "PM" in matches.group(3):
-                h1 = int(h1) + 12
+        if "PM" in matches.group(3):
+            h1 = int(h1) + 12
 
-            if len(str(h1)) == 1:
-                h1 = "0" + str(h1)
+        if len(str(h1)) == 1:
+            h1 = "0" + str(h1)
 
-            if len(m1) == 0:
-                m1 = "00" + m1
+        if len(m1) == 0:
+            m1 = "00" + m1
 
-            if "PM" in matches.group(6):
-                h2 = int(h2) + 12
+        if "PM" in matches.group(6):
+            h2 = int(h2) + 12
 
-            if len(str(h2)) == 1:
-                h2 = "0" + str(h2)
+        if len(str(h2)) == 1:
+            h2 = "0" + str(h2)
 
-            if len(m2) == 0:
-                m2 = "00" + m2
+        if len(m2) == 0:
+            m2 = "00" + m2
 
+        if int(h1) < 24 and int(h2) < 24 and int(m1) < 60 and int(m2) < 60:
             return f"{h1}:{m1} to {h2}:{m2}"
 
-        else:
-            raise ValueError
 
     else:
         raise ValueError
