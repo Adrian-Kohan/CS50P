be18ab03d3fbee9033b796f080d1ae6b3191fee9
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-02 14:14:47 +0000

    Sat, Mar 2, 2024, 5:44 PM +03:30

--- CS50/CS50P/PSET7/working/working.py
+++ CS50/CS50P/PSET7/working/working.py
@@ -7,20 +7,27 @@ def main():
 
 
 def convert(s):
-    h1 = ""
-    m1 = ""
-    h2 = ""
-    m2 = ""
-    time2 = ""
     if matches := re.search(r'^([0-9]{1}[0-9]?):?([0-9]?[0-9]?) (A?P?M) to ([0-9]{1}[0-9]?):?([0-9]?[0-9]?) (A?P?M)$',s):
+
+        h1 = matches.group(1)
+        m1 = matches.group(2)
+        h2 = matches.group(4)
+        m2 = matches.group(5)
+
         if len(matches.group(1)) == 1:
             h1 = 0 + matches.group(1)
-        
-        if "PM" in matches.group(3):
-            time1 = f"{int(matches.group(1)) + 12}:{matches.group(2)}"
 
-        elif "PM" in matches.group(6):
-            time2 = f"{int(matches.group(4)) + 12}:{matches.group(5)}"
+            if "PM" in matches.group(3):
+                return f"{int(h1) + 12}:{m1}"
+
+            else:
+                return f"{h1}:{m1}"
+
+        elif len(matches.group(1)) == 1:
+            h2 = 0 + matches.group(4)
+
+            if "PM" in matches.group(6):
+                time2 = f"{int(matches.group(4)) + 12}:{matches.group(5)}"
 
 
 
