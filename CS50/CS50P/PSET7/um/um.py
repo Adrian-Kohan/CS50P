7c9516e4875b407eadeaff23a7407bdd4a7d3d00
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-02 15:45:18 +0000

    Sat, Mar 2, 2024, 7:15 PM +03:30

---
+++ CS50/CS50P/PSET7/um/um.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    if matches := re.search(r'^.*$',s):
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
