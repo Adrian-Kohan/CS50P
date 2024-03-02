f3adf9eb1332c48022108ac05eeb803e5cdb2df1
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-02 14:38:03 +0000

    Sat, Mar 2, 2024, 6:08 PM +03:30

--- CS50/CS50P/PSET7/working/working.py
+++ CS50/CS50P/PSET7/working/working.py
@@ -11,11 +11,11 @@ def convert(s):
 
         h1 = matches.group(1)
         m1 = matches.group(2)
-        time1 = h1:m1
+        time1 = f"{h1}:{m1}"
 
         h2 = matches.group(4)
         m2 = matches.group(5)
-        time2 = h2:m2
+        time2 = f"{h2}:{m2}"
 
         if len(h1) == 1:
             h1 = "0" + h1
@@ -26,20 +26,16 @@ def convert(s):
         if len(m1) == 0:
             time1 = h1
 
-        
-
-        elif "AM" in matches.group(3):
-                return f"{h1}:{m1}"
-
         if len(h2) == 1:
             h2 = "0" + h2
 
         if "PM" in matches.group(6):
-            return f"{int(h2) + 12}:{m2}"
+            h2 = int(h2) + 12
 
-        elif "AM" in matches.group(6):
-            return f"{h2}:{m2}"
+        if len(m2) == 0:
+            time1 = h1
 
+        
 
     else:
         raise ValueError
