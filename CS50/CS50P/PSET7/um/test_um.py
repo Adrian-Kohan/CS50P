edc1bb875b80c1beff370e5f248233ac2ace6d81
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-02 16:05:27 +0000

    Sat, Mar 2, 2024, 7:35 PM +03:30

--- CS50/CS50P/PSET7/um/test_um.py
+++ CS50/CS50P/PSET7/um/test_um.py
@@ -1,3 +1,11 @@
 from um import count
 
-def test
+def test_only_um():
+    assert count("um ") == 1
+    assert count("hi") == 0
+
+def test_um_in_middle():
+    assert count("yummy") == 0
+
+def test_multiple_um():
+    assert count("Um, thanks, um...") == 2
