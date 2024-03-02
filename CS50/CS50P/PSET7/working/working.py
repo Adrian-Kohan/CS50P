a0755f8563df0f181c8033f32a273432283871a3
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-02 15:24:47 +0000

    Sat, Mar 2, 2024, 6:54 PM +03:30

--- CS50/CS50P/PSET7/working/working.py
+++ CS50/CS50P/PSET7/working/working.py
@@ -14,8 +14,8 @@ def convert(s):
 
         h2 = matches.group(4)
         m2 = matches.group(5)
-        if 
-        if int(h1) < 24 and int(h2) < 24 and int(m1) < 60 and int(m2) < 60:
+        if
+        if int(h1) < 24 and int(h2):
 
             if "PM" in matches.group(3):
                 h1 = int(h1) + 12
