3398bca17057a8a0d79d1751f5c4f16dfcf6699b
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 06:41:34 +0000

    Sun, Mar 3, 2024, 10:11 AM +03:30

--- CS50/CS50P/PSET8/seasons/test_seasons.py
+++ CS50/CS50P/PSET8/seasons/test_seasons.py
@@ -8,5 +8,4 @@ def test_incorrect_date():
     assert convert("1990.24.50") == sys.exit ("Invalid date")
 
 def test_correct_date():
-    if today == "2003-01-01":
-        assert convert("2001-01-01") == "One million, fifty-one thousand, two hundred minutes"
+    assert convert("2001-01-01", "2003-01-01") == "One million, fifty-one thousand, two hundred minutes"
