ee6d4509d47943849ca666056e1aa305a12d5d67
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 06:07:20 +0000

    Sun, Mar 3, 2024, 9:37 AM +03:30

--- CS50/CS50P/PSET8/seasons/seasons.py
+++ CS50/CS50P/PSET8/seasons/seasons.py
@@ -7,37 +7,37 @@ def main():
     user_birth_date = input("Date of birth: ")
     today = date.today()
 
-    try:
-        year, month, day = user_birth_date.split("-")
+    # try:
+    year, month, day = user_birth_date.split("-")
 
-        if birth_date := date(int(year), int(month), int(day)):
+    if birth_date := date(int(year), int(month), int(day)):
 
-            # days = abs(birth_date - today).days
+        # days = abs(birth_date - today).days
 
-            # days_in_min = days * 24 * 60
-            # print(days_in_min)
+        # days_in_min = days * 24 * 60
+        # print(days_in_min)
 
-            # datetime(year, month, day, hour, minute, second)
-            a = datetime(int(year), int(month), int(day))
-            b = datetime(today.year, today.month, today.day)
+        # datetime(year, month, day, hour, minute, second)
+        a = date(int(year), int(month), int(day))
+        b = date(today.year, today.month, today.day)
 
-            # returns a timedelta object
-            c = a-b
+        # returns a timedelta object
+        c = a-b
 
-            minutes = c.total_seconds() / 60
-            print('Total difference in minutes: ', minutes)
+        minutes = c.total_seconds() / 60
+        print('Total difference in minutes: ', minutes)
 
-            # returns the difference of the time of the day
-            minutes = c.seconds / 60
-            print('Difference in minutes: ', minutes)
+        # returns the difference of the time of the day
+        minutes = c.seconds / 60
+        print('Difference in minutes: ', minutes)
 
-            # inflector = inflect.engine()
-            # words = inflector.number_to_words(days_in_min)
-            # print(words)
+        # inflector = inflect.engine()
+        # words = inflector.number_to_words(days_in_min)
+        # print(words)
 
 
-    except:
-        sys.exit ("Invalid date")
+    # except:
+    #     sys.exit ("Invalid date")
 
 
 if __name__ == "__main__":
