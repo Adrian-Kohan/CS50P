a6712a54300cbd0f74b07e04ebbded394ab7bb80
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 05:32:10 +0000

    Sun, Mar 3, 2024, 9:02 AM +03:30

--- CS50/CS50P/PSET8/seasons/seasons.py
+++ CS50/CS50P/PSET8/seasons/seasons.py
@@ -4,21 +4,18 @@ import sys
 
 def main():
     try:
-        user_birth_date = date(input("Date of birth: "))
+        user_birth_date = ]input("Date of birth: "))
         today = date.today()
 
         year1, month1, day1 = user_birth_date.split("-")
         year2, month2, day2 = today.split("-")
 
-        diff_year = year2 -year1
-        diff_month = month2 - month1
-        diff_day = day2 - day1
 
-        time_delta1 = timedelta(years= days=23, hours=0, minutes=20)
-        time_delta2 = timedelta(days=15, seconds=2, microseconds=123, milliseconds=234566, minutes=5, hours=2)
+        time_delta1 = timedelta(year1, month1, day1)
+        time_delta2 = timedelta(year2, month2, day2)
 
         result = time_delta1 - time_delta2
-        print(today)
+        print(result)
     except:
         sys.exit "Invalid date"
 
