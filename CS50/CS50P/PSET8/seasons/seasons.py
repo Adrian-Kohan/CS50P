86b805104737b62e38adb1389846b2bea73af7f2
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 05:55:43 +0000

    Sun, Mar 3, 2024, 9:25 AM +03:30

--- CS50/CS50P/PSET8/seasons/seasons.py
+++ CS50/CS50P/PSET8/seasons/seasons.py
@@ -13,6 +13,7 @@ def main():
         if birth_date := date(int(year), int(month), int(day)):
             days = abs(birth_date - today).days
             days_in_min = days * 24 * 60
+            print(days_in_min)
 
             inflector = inflect.engine()
             words = inflector.number_to_words(days_in_min)
