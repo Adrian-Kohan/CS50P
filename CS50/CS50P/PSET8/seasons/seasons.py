d82f7c11f041b5963afea54546a14edba8f13446
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 05:03:36 +0000

    Sun, Mar 3, 2024, 8:33 AM +03:30

---
+++ CS50/CS50P/PSET8/seasons/seasons.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+
+def main():
+    user_birth_date = input("Date of birth: ")
+    today = date.today()
+    
+    print(today)
+
+
+
+
+if __name__ == "__main__":
+    main()
