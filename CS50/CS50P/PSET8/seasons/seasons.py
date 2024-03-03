7eb292418c83a74c3b1f770ed35d8f6c8ea8ed0f
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 05:26:59 +0000

    Sun, Mar 3, 2024, 8:56 AM +03:30

--- CS50/CS50P/PSET8/seasons/seasons.py
+++ CS50/CS50P/PSET8/seasons/seasons.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 import sys
 
 
@@ -9,7 +9,15 @@ def main():
 
         year1, month1, day1 = user_birth_date.split("-")
         year2, month2, day2 = today.split("-")
-        years_in_min = (year2 -year1) * 365
+
+        diff_year = year2 -year1
+        diff_month = month2 - month1
+        diff_day = day2 - day1
+
+        time_delta1 = timedelta(years= days=23, hours=0, minutes=20)
+        time_delta2 = timedelta(days=15, seconds=2, microseconds=123, milliseconds=234566, minutes=5, hours=2)
+
+        result = time_delta1 - time_delta2
         print(today)
     except:
         sys.exit "Invalid date"
