a56b13db166646129d99c4b6565660379593e50c
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 05:19:17 +0000

    Sun, Mar 3, 2024, 8:49 AM +03:30

--- CS50/CS50P/PSET8/seasons/seasons.py
+++ CS50/CS50P/PSET8/seasons/seasons.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     try:
-        user_birth_date = input("Date of birth: ")
+        user_birth_date = date(input("Date of birth: "))
         today = date.today()
 
         year1, month1, day1 = user_birth_date.split("-")
