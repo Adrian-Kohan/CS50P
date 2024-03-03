c69b9e5dd39199ad6123983a81639f5b530f27e9
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 06:23:52 +0000

    Sun, Mar 3, 2024, 9:53 AM +03:30

--- CS50/CS50P/PSET8/seasons/seasons.py
+++ CS50/CS50P/PSET8/seasons/seasons.py
@@ -25,7 +25,7 @@ def convert(date1, date2):
             days_in_min = days * 24 * 60
 
             # turn numerical numbers to English words
-            words = inflector.number_to_words(days_in_min)
+            words = inflector.number_to_words(days_in_min).captalize()
             words = words.replace(" and", ",")
             return words
 
