1faec86c6172a2b664b3fe51106e37acc4aff18c
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 05:52:42 +0000

    Sun, Mar 3, 2024, 9:22 AM +03:30

--- CS50/CS50P/PSET8/seasons/seasons.py
+++ CS50/CS50P/PSET8/seasons/seasons.py
@@ -1,5 +1,6 @@
-from datetime import date, timedelta
+from datetime import date
 import sys
+import inflect
 
 
 def main():
@@ -12,13 +13,14 @@ def main():
         if birth_date := date(int(year), int(month), int(day)):
             days = abs(birth_date - today).days
             days_in_min = days * 24 * 60
-            
 
+            inflector = inflect.engine()
+            words = inflector.number_to_words(days_in_min)
+            print(words)
 
 
     except:
         sys.exit ("Invalid date")
-
 
 
 if __name__ == "__main__":
