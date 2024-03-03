e41627853490c13cc4ef9d7a059956efe08f015b
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 05:34:20 +0000

    Sun, Mar 3, 2024, 9:04 AM +03:30

--- CS50/CS50P/PSET8/seasons/seasons.py
+++ CS50/CS50P/PSET8/seasons/seasons.py
@@ -3,21 +3,23 @@ import sys
 
 
 def main():
+    user_birth_date = input("Date of birth: ")
+    today = date.today()
+
     try:
-        user_birth_date = ]input("Date of birth: "))
-        today = date.today()
+        if date(user_birth_date):
 
-        year1, month1, day1 = user_birth_date.split("-")
-        year2, month2, day2 = today.split("-")
+            year1, month1, day1 = user_birth_date.split("-")
+            year2, month2, day2 = today.split("-")
 
 
-        time_delta1 = timedelta(year1, month1, day1)
-        time_delta2 = timedelta(year2, month2, day2)
+            time_delta1 = timedelta(year1, month1, day1)
+            time_delta2 = timedelta(year2, month2, day2)
 
-        result = time_delta1 - time_delta2
-        print(result)
+            result = time_delta1 - time_delta2
+            print(result)
     except:
-        sys.exit "Invalid date"
+        sys.exit ("Invalid date")
 
 
 
