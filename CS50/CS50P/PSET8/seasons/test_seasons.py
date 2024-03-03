912c08b591b46ebb89353cb94adc0fe7cea475d9
Adrian kohan <139708915+Adrian-Kohan@users.noreply.github.com>
2024-03-03 07:00:51 +0000

    Sun, Mar 3, 2024, 10:30 AM +03:30

--- CS50/CS50P/PSET8/seasons/test_seasons.py
+++ CS50/CS50P/PSET8/seasons/test_seasons.py
@@ -1,12 +1,14 @@
 from seasons import convert
 from datetime import date
+import pytest
 
 def test_incorrect_date_format():
-    with Raises(SystemExit):
+    with pytest.raises(SystemExit):
         assert convert("1990.2.2", date(2003, 1, 1))
 
 def test_incorrect_date():
-    assert convert("1990.24.50", date(2003, 1, 1)) == "Invalid date"
+    with pytest.raises(SystemExit):
+        assert convert("1990.24.50", date(2003, 1, 1)) == "Invalid date"
 
 def test_correct_date():
     assert convert("2001-1-1", date(2003, 1, 1)) == "One million, fifty-one thousand, two hundred minutes"
